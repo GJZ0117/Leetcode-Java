9a1e922d4928f6f7a210f2617f2111426bb06928
GJZ <gjz0117@163.com>
2022-10-03 21:03:32 +0800

    9. Palindrome Number

---
+++ easy/Palindrome_Number.java
@@ -0,0 +1,35 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 9. Palindrome Number
+ */
+
+public class Palindrome_Number {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        } else if (x == 0) {
+            return true;
+        }
+        List<Integer> list = new ArrayList<>();
+        int cur;
+        while (x > 0) {
+            cur = x % 10;
+            list.add(cur);
+            x /= 10;
+        }
+        int i = 0;
+        int j = list.size() - 1;
+        while (i < j) {
+            if (list.get(i) != list.get(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
