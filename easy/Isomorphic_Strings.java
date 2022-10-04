b01b9a0c1c81cf2eed95f4a91763a5c99c0d344a
GJZ <gjz0117@163.com>
2022-10-04 15:16:20 +0800

    205. Isomorphic Strings

---
+++ easy/Isomorphic_Strings.java
@@ -0,0 +1,45 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 205. Isomorphic Strings
+ */
+
+public class Isomorphic_Strings {
+
+    public static void main(String[] args) {
+        String s = "egg";
+        String t = "add";
+        System.out.println(isIsomorphic(s, t));
+    }
+
+    public static boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        } else if (s == null || s.length() == 0) {
+            return true;
+        }
+        Map<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+            if (map.containsKey(a)) {
+                if (map.get(a).equals(b)) {
+                    continue;
+                } else {
+                    return false;
+                }
+            } else {
+                if (!map.containsValue(b)) {
+                    map.put(a, b);
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
