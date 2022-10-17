0065ad09f9cabfbbd1a0400a9ebce0db1b920130
GJZ <gjz0117@163.com>
2022-10-17 11:06:29 +0800

    100. Same Tree

---
+++ easy/Same_Tree.java
@@ -0,0 +1,20 @@
+package easy;
+
+/**
+ * 100. Same Tree
+ */
+
+public class Same_Tree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else {
+            if (p.val != q.val) {
+                return false;
+            }
+            return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        }
+    }
+}
