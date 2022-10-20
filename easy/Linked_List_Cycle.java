0a98b79d6565ef75c9978ba5c95ff7c481032d63
GJZ <gjz0117@163.com>
2022-10-20 10:04:49 +0800

    141. Linked List Cycle

---
+++ easy/Linked_List_Cycle.java
@@ -0,0 +1,23 @@
+package easy;
+
+/**
+ * 141. Linked List Cycle
+ */
+
+public class Linked_List_Cycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
