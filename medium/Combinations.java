6570936b84ab8209906a0f20c666ad224ff1fb69
GJZ <gjz0117@163.com>
2022-10-26 10:49:58 +0800

    77. Combinations

---
+++ medium/Combinations.java
@@ -0,0 +1,40 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 77. Combinations
+ */
+
+public class Combinations {
+
+    public static void main(String[] args) {
+        int n = 1;
+        int k = 1;
+        List<List<Integer>> lists = combine(n, k);
+        for (List<Integer> list : lists) {
+            System.out.println(list);
+        }
+    }
+
+    public static List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        generate(ans, list, n, k, 1);
+        return ans;
+    }
+
+    public static void generate(List<List<Integer>> ans, List<Integer> list, int n, int k, int start) {
+        if (list.size() == k) {
+            List<Integer> temp = new ArrayList<>(list);
+            ans.add(temp);
+            return;
+        }
+        for (int i = start; i <= n; i++) {
+            list.add(i);
+            generate(ans, list, n, k, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+}
