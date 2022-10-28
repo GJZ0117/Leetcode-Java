8e1022fcd8d1135d464ea6ab754d5e36e091b69c
GJZ <gjz0117@163.com>
2022-10-28 10:34:01 +0800

    86. Partition List

---
+++ medium/Partition_List.java
@@ -0,0 +1,33 @@
+package medium;
+
+/**
+ * 86. Partition List
+ */
+
+public class Partition_List {
+    public ListNode partition(ListNode head, int x) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode list1 = new ListNode();
+        ListNode list2 = new ListNode();
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+        ListNode cur = head;
+        while (cur != null) {
+            if (cur.val < x) { // lower cases put into list1
+                temp1.next = cur;
+                temp1 = temp1.next;
+                cur = cur.next;
+                temp1.next = null;
+            } else { // higher cases put into list2
+                temp2.next = cur;
+                temp2 = temp2.next;
+                cur = cur.next;
+                temp2.next = null;
+            }
+        }
+        temp1.next = list2.next; // connect list1 and list2
+        return list1.next;
+    }
+}
