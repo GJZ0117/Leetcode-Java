d5db43680961d7cf9b5a640889bb5da55f5a0981
GJZ <gjz0117@163.com>
2022-10-31 10:11:22 +0800

    97. Interleaving String

---
+++ medium/Interleaving_String.java
@@ -0,0 +1,66 @@
+package medium;
+
+/**
+ * 97. Interleaving String
+ */
+
+public class Interleaving_String {
+
+    public static void main(String[] args) {
+        String s1 = "aabcc";
+        String s2 = "dbbca";
+        String s3 = "aadbbcbcac";
+        System.out.println(isInterleave(s1, s2, s3));
+    }
+
+    //dp: dp(i,j) = [dp(i-1,j) and s1(i-1)=s3(k)] or [dp(i,j-1) and s2(j-1)=s3(k)], k = i + j - 1
+    public boolean betterSolution(String s1, String s2, String s3) {
+        int n = s1.length();
+        int m = s2.length();
+        int t = s3.length();
+        if (n + m != t) {
+            return false;
+        }
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        dp[0][0] = true; // dp[0][0] refers to "" and ""
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                int k = i + j - 1;
+                if (i > 0) {
+                    dp[i][j] = dp[i][j] || (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(k));
+                }
+                if (j > 0) {
+                    dp[i][j] = dp[i][j] || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(k));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+
+    //Time Limit Exceeded
+    public static boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+        return search(s1, s2, s3, 0, 0, 0);
+    }
+
+
+    public static boolean search(String s1, String s2, String s3, int i, int j, int k) {
+        if (k >= s3.length()) {
+            return true;
+        }
+        if (i < s1.length() && s1.charAt(i) == s3.charAt(k)) {
+            if (search(s1, s2, s3, i + 1, j, k + 1)) {
+                return true;
+            }
+        }
+        if (j < s2.length() && s2.charAt(j) == s3.charAt(k)) {
+            if (search(s1, s2, s3, i, j + 1, k + 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
