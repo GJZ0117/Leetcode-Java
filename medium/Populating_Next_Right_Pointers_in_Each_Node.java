2fe2bd26113e3a596ac6c8eb1969781e9a536c04
GJZ <gjz0117@163.com>
2022-11-03 12:09:01 +0800

    116. Populating Next Right Pointers in Each Node

--- medium/Populating_Next_Right_Pointers_in_Each_Node.java
+++ medium/Populating_Next_Right_Pointers_in_Each_Node.java
@@ -29,6 +29,22 @@ class Node {
 };
 
 public class Populating_Next_Right_Pointers_in_Each_Node {
+
+    // find each node's father node and check its father's next pointer
+    public Node betterSolution(Node root) {
+        dfs(root, null);
+        return root;
+    }
+
+    private void dfs(Node curr, Node next) {
+        if (curr == null) return;
+        curr.next = next;
+        dfs(curr.left, curr.right);
+        dfs(curr.right, curr.next == null ? null : curr.next.left);
+    }
+
+
+    //cost too much space
     public Node connect(Node root) {
         if (root == null) {
             return null;
