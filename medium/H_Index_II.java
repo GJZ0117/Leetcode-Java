64fe4e3ffe3e5ed6f28ea68654a79df4c5722d7e
GJZ <gjz0117@163.com>
2022-11-24 10:23:29 +0800

    275. H-Index II

---
+++ medium/H_Index_II.java
@@ -0,0 +1,21 @@
+package medium;
+
+/**
+ * 275. H-Index II
+ */
+
+public class H_Index_II {
+    public int hIndex(int[] citations) {
+        int left = 0;
+        int right = citations.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (citations[mid] >= citations.length - mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return citations.length - left;
+    }
+}
