ab581538706d22099082ff11dc8bb7896f08ff8e
GJZ <gjz0117@163.com>
2022-11-27 09:46:28 +0800

    304. Range Sum Query 2D - Immutable

---
+++ medium/NumMatrix.java
@@ -0,0 +1,52 @@
+package medium;
+
+/**
+ * 304. Range Sum Query 2D - Immutable
+ */
+
+public class NumMatrix {
+
+    int m;
+    int n;
+    int[][] dp;
+
+    public NumMatrix(int[][] matrix) {
+        m = matrix.length;
+        n = matrix[0].length;
+        dp = new int[m][n];
+        dp[0][0] = matrix[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = matrix[i][0] + dp[i - 1][0];
+        }
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = matrix[0][i] + dp[0][i - 1];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = matrix[i][j] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
+            }
+        }
+        print(dp);
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        if (row1 == 0 && col1 == 0) {
+            return dp[row2][col2];
+        } else if (row1 == 0) {
+            return dp[row2][col2] - dp[row2][col1 - 1];
+        } else if (col1 == 0) {
+            return dp[row2][col2] - dp[row1 - 1][col2];
+        } else {
+            return dp[row2][col2] - dp[row2][col1 - 1] - dp[row1 - 1][col2] + dp[row1 - 1][col1 - 1];
+        }
+    }
+
+    public void print(int[][] dp) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
