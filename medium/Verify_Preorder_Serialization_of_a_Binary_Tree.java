af9878234591dd6720a239ca342f991168f2124d
GJZ <gjz0117@163.com>
2022-11-30 20:45:53 +0800

    331. Verify Preorder Serialization of a Binary Tree

---
+++ medium/Verify_Preorder_Serialization_of_a_Binary_Tree.java
@@ -0,0 +1,41 @@
+package medium;
+
+import java.util.Stack;
+
+/**
+ * 331. Verify Preorder Serialization of a Binary Tree
+ */
+
+public class Verify_Preorder_Serialization_of_a_Binary_Tree {
+    //https://leetcode.cn/problems/verify-preorder-serialization-of-a-binary-tree/solution/pai-an-jiao-jue-de-liang-chong-jie-fa-zh-66nt/
+
+    // put every node include null into stack, when there are two continuous null push into stack, then pop them and the precious node. Add one null into stack. (pop leaf nodes and push a null)
+    public boolean isValidSerialization1(String preorder) {
+        Stack<String> stack = new Stack<>();
+        for (String str : preorder.split(",")) {
+            stack.push(str);
+            while (stack.size() >= 3 && stack.get(stack.size() - 1).equals("#") && stack.get(stack.size() - 2).equals("#")) {
+                stack.pop();
+                stack.pop();
+                stack.pop();
+                stack.push("#");
+            }
+        }
+        return (stack.size() == 1 && stack.peek().equals("#"));
+    }
+
+    // if a node is not null, it has one in-degree and two out-dgree (root node has 0 in-degree)
+    public boolean isValidSerialization2(String preorder) {
+        int diff = 1; //eliminate root node's in-degree
+        for (String str : preorder.split(",")) {
+            diff -= 1;
+            if (diff < 0) {
+                return false;
+            }
+            if (!str.equals("#")) {
+                diff += 2;
+            }
+        }
+        return diff == 0;
+    }
+}
