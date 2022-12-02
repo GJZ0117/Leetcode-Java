1acc2fa3e555ab9fcab2f9cd430da8914fbf1ff2
GJZ <gjz0117@163.com>
2022-12-02 17:49:14 +0800

    343. Integer Break

--- medium/Integer_Break.java
+++ medium/Integer_Break.java
@@ -7,8 +7,9 @@ package medium;
 public class Integer_Break {
 
     public static void main(String[] args) {
-        int n = 20;
-        System.out.println(_integerBreak(n));
+        for (int n = 2; n < 20; n++) {
+            System.out.println(integerBreak(n) + " " + _integerBreak(n));
+        }
     }
 
     public static int integerBreak(int n) {
@@ -25,19 +26,19 @@ public class Integer_Break {
     //Time Limit Exceeded
     public static int _integerBreak(int n) {
         int[] res = {0};
-        dfs(n, 1, res);
+        dfs(n, n, 1, res);
         return res[0];
     }
 
-    public static void dfs(int remain, int tempRes, int[] res) {
+    public static void dfs(int n, int remain, int tempRes, int[] res) {
         if (remain == 0) {
             res[0] = Math.max(res[0], tempRes);
             return;
         } else if (remain < 0) {
             return;
         }
-        for (int i = 1; i <= remain; i++) {
-            dfs(remain - i, tempRes * i, res);
+        for (int i = 1; i <= (remain == n ? remain - 1 : remain); i++) {
+            dfs(n, remain - i, tempRes * i, res);
         }
     }
 }
