a176a5eae4b0ac5d4efc8366b2ac149077ed2627
GJZ <gjz0117@163.com>
2022-12-03 11:22:18 +0800

    357. Count Numbers with Unique Digits

---
+++ medium/Count_Numbers_with_Unique_Digits.java
@@ -0,0 +1,44 @@
+package medium;
+
+/**
+ * 357. Count Numbers with Unique Digits
+ */
+
+public class Count_Numbers_with_Unique_Digits {
+
+    public static void main(String[] args) {
+        for (int n = 0; n <= 8; n++) {
+            System.out.println("ans : " + countNumbersWithUniqueDigits(n));
+        }
+    }
+
+    public static int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) {
+            return 1;
+        } else if (n == 1) {
+            return 10;
+        }
+
+        int[] records = new int[n+1];
+        records[1] = 10;
+        int tempAns = 9;
+        int pre = 9;
+
+        for (int i = 2; i <= n; i++) {
+            if (i == 2) {
+                tempAns = tempAns * 9;
+                pre = 9;
+            } else {
+                tempAns = tempAns * (pre - 1);
+                pre--;
+            }
+            records[i] = tempAns;
+        }
+
+        int ans = 0;
+        for (int num : records) {
+            ans = ans + num;
+        }
+        return ans;
+    }
+}
