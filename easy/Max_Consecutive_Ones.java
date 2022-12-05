84f0bf7ef6daf7d338b92884e74020aca3e8667c
GJZ <gjz0117@163.com>
2022-12-05 19:37:54 +0800

    485. Max Consecutive Ones

---
+++ easy/Max_Consecutive_Ones.java
@@ -0,0 +1,40 @@
+package easy;
+
+import java.util.Arrays;
+
+/**
+ * 485. Max Consecutive Ones
+ */
+
+public class Max_Consecutive_Ones {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 0, 1, 1, 1};
+        System.out.println(Arrays.toString(nums));
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxLen = 0;
+        int start = -1;
+        int end = -1;
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+            System.out.println(start + " " + end + " " + maxLen);
+            if (nums[i] == 1 && start == -1) {
+                start = i;
+                end = i;
+            } else if (nums[i] == 1 && start != -1) {
+                end = i;
+            } else if (nums[i] == 0 && start != -1) {
+                maxLen = Math.max(maxLen, end - start + 1);
+                start = -1;
+            }
+            System.out.println(start + " " + end + " " + maxLen);
+            System.out.println("-----------------");
+        }
+        if (start != -1) {
+            maxLen = Math.max(maxLen, end - start + 1);
+        }
+        return maxLen;
+    }
+}
