5151783de905063bcb5b41a4b1b84e7bc48794b7
GJZ <gjz0117@163.com>
2022-12-10 16:53:41 +0800

    390. Elimination Game

---
+++ easy/Elimination_Game.java
@@ -0,0 +1,32 @@
+package easy;
+
+/**
+ * 390. Elimination Game
+ */
+
+public class Elimination_Game {
+
+    //https://leetcode.cn/problems/elimination-game/solution/c-shu-xue-by-qian2-60yk/
+    public int lastRemaining(int n) {
+        int numAccount = n;
+        int loopCount = 0;
+        int a0 = 1;
+        int d = 1;
+        while (numAccount != 1) {
+            if (numAccount % 2 == 1) {
+                a0 = a0 + d;
+            } else if (numAccount % 2 == 0) {
+                boolean leftToRight = ((loopCount % 2) == 0);
+                if (leftToRight) {
+                    a0 = a0 + d;
+                } else {
+                    a0 = a0;
+                }
+            }
+            loopCount++;
+            d *= 2;
+            numAccount /= 2;
+        }
+        return a0;
+    }
+}
