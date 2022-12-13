a8b31112e53a0736016aaaf00ed2aaa0ed55b6dd
GJZ <gjz0117@163.com>
2022-12-13 22:27:09 +0800

    521. Longest Uncommon Subsequence I

---
+++ easy/Longest_Uncommon_Subsequence_I.java
@@ -0,0 +1,14 @@
+package easy;
+
+/**
+ * 521. Longest Uncommon Subsequence I
+ */
+
+public class Longest_Uncommon_Subsequence_I {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        }
+        return Math.max(a.length(), b.length());
+    }
+}
