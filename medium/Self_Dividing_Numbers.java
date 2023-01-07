6689835a5e3b6b0aae9362bbf2f303433c593e87
GJZ <gjz0117@163.com>
2023-01-07 10:01:24 +0800

    728. Self Dividing Numbers

---
+++ medium/Self_Dividing_Numbers.java
@@ -0,0 +1,35 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 728. Self Dividing Numbers
+ */
+
+public class Self_Dividing_Numbers {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            if (check(i)) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+    public boolean check(int num) {
+        int temp = num;
+        while (temp > 0) {
+            int n = temp % 10;
+            temp /= 10;
+            if (n == 0 && temp != 0) {
+                return false;
+            }
+            if (num % n != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
