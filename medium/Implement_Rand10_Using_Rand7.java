241a26ca3ff71689b404605c0593cea6dd22e2ff
GJZ <gjz0117@163.com>
2023-01-09 20:07:25 +0800

    470. Implement Rand10() Using Rand7()

---
+++ medium/Implement_Rand10_Using_Rand7.java
@@ -0,0 +1,27 @@
+package medium;
+
+import java.util.Random;
+
+/**
+ * 470. Implement Rand10() Using Rand7()
+ */
+
+public class Implement_Rand10_Using_Rand7 {
+    public int rand10() {
+        int base1 = rand7();
+        while (base1 == 7) {
+            base1 = rand7();
+        }
+        base1 = base1 % 2;
+        int base2 = rand7();
+        while (base2 >= 6) {
+            base2 = rand7();
+        }
+        return base2 + 5 * base1;
+    }
+
+    public int rand7() {
+        Random random = new Random();
+        return random.nextInt(1, 7);
+    }
+}
