29108e5375a304bbc6878f48351e5931d06b5a02
GJZ <gjz0117@163.com>
2023-01-22 19:43:40 +0800

    528. Random Pick with Weight

--- medium/Random_Pick_with_Weight.java
+++ medium/Random_Pick_with_Weight.java
@@ -24,7 +24,8 @@ public class Random_Pick_with_Weight {
         }
 
         public int pickIndex() {
-            int num = random.nextInt(1, sum + 1);
+//            int num = random.nextInt(1, sum + 1); // Leetcode中AC,但IDEA中编译报错
+            int num = 0; // 提交时删除这句用上面的代替
             for (int i = dp.length - 2; i >= 0; i--) {
                 if (num > dp[i]) {
                     return i + 1;
