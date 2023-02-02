4755976cff113de4c81ce3e501a35c9d8ab6f58b
GJZ <gjz0117@163.com>
2023-02-02 10:25:30 +0800

    867. Transpose Matrix

---
+++ easy/Transpose_Matrix.java
@@ -0,0 +1,19 @@
+package easy;
+
+/**
+ * 867. Transpose Matrix
+ */
+
+public class Transpose_Matrix {
+    public int[][] transpose(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] ans = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        return ans;
+    }
+}
