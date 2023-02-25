c7404790af86c63ddc96487915133b78fd515d4f
GJZ <gjz0117@163.com>
2023-02-25 11:43:49 +0800

    669. Trim a Binary Search Tree

---
+++ medium/Trim_a_Binary_Search_Tree.java
@@ -0,0 +1,22 @@
+package medium;
+
+/**
+ * 669. Trim a Binary Search Tree
+ */
+
+public class Trim_a_Binary_Search_Tree {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        }
+        if (root.val > high) {
+            return trimBST(root.left, low, high);
+        }
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+}
