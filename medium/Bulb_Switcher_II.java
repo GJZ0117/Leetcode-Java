dcb0580086a2675aae04de1c82a408580e88d698
GJZ <gjz0117@163.com>
2023-02-26 11:15:50 +0800

    672. Bulb Switcher II

---
+++ medium/Bulb_Switcher_II.java
@@ -0,0 +1,21 @@
+package medium;
+
+/**
+ * 672. Bulb Switcher II
+ */
+
+public class Bulb_Switcher_II {
+    // https://leetcode.cn/problems/bulb-switcher-ii/solution/dengp-by-capital-worker-51rb/
+    public int flipLights(int n, int presses) {
+        if (presses == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 2;
+        } else if (n == 2) {
+            return presses == 1 ? 3 : 4;
+        } else {
+            return presses == 1 ? 4 : presses == 2 ? 7 : 8;
+        }
+    }
+}
