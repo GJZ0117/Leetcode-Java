f77c344c56ac28c35849c29eb969e217cfe77909
GJZ <gjz0117@163.com>
2023-03-23 18:32:12 +0800

    763. Partition Labels

--- medium/Partition_Labels.java
+++ medium/Partition_Labels.java
@@ -9,6 +9,28 @@ import java.util.*;
 public class Partition_Labels {
     public List<Integer> partitionLabels(String s) {
         List<Integer> ans = new ArrayList<>();
+        int[] arr = new int[26];
+        Arrays.fill(arr, -1);
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            arr[chars[i] - 'a'] = i;
+        }
+        int start = 0;
+        int scannedCharMaxPosition = 0;
+        for (int i = 0; i < chars.length; i++) {
+            int curCharMaxPosition = arr[chars[i] - 'a'];
+            scannedCharMaxPosition = Math.max(scannedCharMaxPosition, curCharMaxPosition);
+            if (i == scannedCharMaxPosition) {
+                ans.add(i - start + 1);
+                start = i + 1;
+            }
+        }
+        return ans;
+    }
+
+
+    public List<Integer> _partitionLabels(String s) {
+        List<Integer> ans = new ArrayList<>();
         Map<Character, Integer> map = new HashMap<>();
         char[] chars = s.toCharArray();
         for (char c : chars) {
