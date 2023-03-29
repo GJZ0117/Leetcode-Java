66e5cb089db347270b1dbad6579e35a7c464c8c1
GJZ <gjz0117@163.com>
2023-03-29 19:45:08 +0800

    786. K-th Smallest Prime Fraction

--- medium/Kth_Smallest_Prime_Fraction.java
+++ medium/Kth_Smallest_Prime_Fraction.java
@@ -7,7 +7,26 @@ import java.util.*;
  */
 
 public class Kth_Smallest_Prime_Fraction {
+
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> arr[a[0]] * arr[b[1]] - arr[b[0]] * arr[a[1]]);
+        for (int j = 1; j < arr.length; j++) {
+            heap.offer(new int[]{0, j});
+        }
+        while (true) {
+            int[] pos = heap.poll();
+            if (--k <= 0) {
+                return new int[]{arr[pos[0]], arr[pos[1]]};
+            }
+            int i = pos[0];
+            int j = pos[1];
+            if (i + 1 < j) {
+                heap.offer(new int[]{i + 1, j});
+            }
+        }
+    }
+
+    public int[] _kthSmallestPrimeFraction(int[] arr, int k) {
         int n = arr.length;
         Map<Double, int[]> map = new HashMap<>(n * (n - 1) / 2);
         List<Double> list = new ArrayList<>(n * (n - 1) / 2);
