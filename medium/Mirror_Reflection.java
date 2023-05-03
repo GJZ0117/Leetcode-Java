68e50a01581665ad9059568898a0d3f820f9744c
GJZ <gjz0117@163.com>
2023-05-03 19:41:23 +0800

    858. Mirror Reflection

---
+++ medium/Mirror_Reflection.java
@@ -0,0 +1,21 @@
+package medium;
+
+/**
+ * 858. Mirror Reflection
+ */
+
+public class Mirror_Reflection {
+    public int mirrorReflection(int p, int q) {
+        while (p % 2 == 0 && q % 2 == 0) {
+            p /= 2;
+            q /= 2;
+        }
+        if (p % 2 == 0) {
+            return 2;
+        }
+        if (q % 2 == 0) {
+            return 0;
+        }
+        return 1;
+    }
+}
