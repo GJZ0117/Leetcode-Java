825f9f9629ca482e79037203a8cfc29318463773
GJZ <gjz0117@163.com>
2023-05-11 22:54:43 +0800

    877. Stone Game

---
+++ medium/Stone_Game.java
@@ -0,0 +1,22 @@
+package medium;
+
+/**
+ * 877. Stone Game
+ */
+
+public class Stone_Game {
+    // https://leetcode.cn/problems/stone-game/solution/shi-zi-you-xi-dong-tai-gui-hua-qu-jian-d-5ra8/
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = piles[i];
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1] > 0;
+    }
+}
