b71d2d4442cead9d32488bb33988b13a10e27e76
GJZ <gjz0117@163.com>
2023-05-29 18:06:20 +0800

    912. Sort an Array

---
+++ medium/Sort_an_Array.java
@@ -0,0 +1,38 @@
+package medium;
+
+/**
+ * 912. Sort an Array
+ */
+
+public class Sort_an_Array {
+    public int[] sortArray(int[] nums) {
+        for (int i = (nums.length - 1) / 2; i >= 0; i--) {
+            adjustHeap(nums, i, nums.length);
+        }
+        for (int i = nums.length - 1; i > 0; i--) {
+            int temp = nums[i];
+            nums[i] = nums[0];
+            nums[0] = temp;
+            adjustHeap(nums, 0, i);
+        }
+        return nums;
+    }
+
+    public void adjustHeap(int[] nums, int parent, int length) {
+        int temp = nums[parent];
+        int lChild = parent * 2 + 1;
+        while (lChild < length) {
+            int rChild = lChild + 1;
+            if (rChild < length && nums[lChild] < nums[rChild]) {
+                lChild++;
+            }
+            if (temp >= nums[lChild]) {
+                break;
+            }
+            nums[parent] = nums[lChild];
+            parent = lChild;
+            lChild = 2 * lChild + 1;
+        }
+        nums[parent] = temp;
+    }
+}
