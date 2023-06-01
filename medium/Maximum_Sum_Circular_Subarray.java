56c6af53bdbf9168069a9ee016cbedcb86970e0b
GJZ <gjz0117@163.com>
2023-06-01 17:04:12 +0800

    918. Maximum Sum Circular Subarray

---
+++ medium/Maximum_Sum_Circular_Subarray.java
@@ -0,0 +1,24 @@
+package medium;
+
+/**
+ * 918. Maximum Sum Circular Subarray
+ */
+
+public class Maximum_Sum_Circular_Subarray {
+    // https://leetcode.cn/problems/maximum-sum-circular-subarray/solution/wo-hua-yi-bian-jiu-kan-dong-de-ti-jie-ni-892u/
+    public int maxSubarraySumCircular(int[] nums) {
+        int total = 0;
+        int maxSum = nums[0];
+        int curMax = 0;
+        int minSum = nums[0];
+        int curMin = 0;
+        for (int num : nums) {
+            curMax = Math.max(curMax + num, num);
+            maxSum = Math.max(maxSum, curMax);
+            curMin = Math.min(curMin + num, num);
+            minSum = Math.min(minSum, curMin);
+            total += num;
+        }
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+    }
+}
