86c559f116686bb23ed4d591d91114cd96f2bd6c
GJZ <gjz0117@163.com>
2023-06-22 12:24:59 +0800

    951. Flip Equivalent Binary Trees

---
+++ medium/Flip_Equivalent_Binary_Trees.java
@@ -0,0 +1,20 @@
+package medium;
+
+/**
+ * 951. Flip Equivalent Binary Trees
+ */
+
+public class Flip_Equivalent_Binary_Trees {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if (root1 != null && root2 != null) {
+            if (root1.val != root2.val) {
+                return false;
+            }
+            return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) || (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
+        } else if (root1 == null && root2 == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
