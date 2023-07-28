6b29bd8fbd1e6ea20fada47fcb7c625458d6662d
GJZ0117 <435390736@qq.com>
2023-07-28 11:21:01 +0800

    1004. Max Consecutive Ones III

---
+++ medium/Max_Consecutive_Ones_III.java
@@ -0,0 +1,28 @@
+package medium;
+
+/**
+ * 1004. Max Consecutive Ones III
+ */
+
+public class Max_Consecutive_Ones_III {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0;
+        int right = 0;
+        int zeroCount = 0;
+        int maxLength = 0;
+        while (right < nums.length) {
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+            while (zeroCount > k) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+            maxLength = Math.max(maxLength, right - left + 1);
+            right++;
+        }
+        return maxLength;
+    }
+}
