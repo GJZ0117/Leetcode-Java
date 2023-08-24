5aef61f62b18425efeb8339f3144363b974b80c2
GJZ0117 <435390736@qq.com>
2023-08-24 10:31:05 +0800

    15. 3Sum

--- medium/ThreeSum.java
+++ medium/ThreeSum.java
@@ -23,7 +23,7 @@ public class ThreeSum {
             j = i + 1;
             k = nums.length - 1;
             while (j < k) {
-                if (nums[i]+nums[j]+nums[k] == 0){
+                if (nums[i] + nums[j] + nums[k] == 0) {
                     List<Integer> list = new ArrayList<>();
                     list.add(nums[i]);
                     list.add(nums[j]);
@@ -31,14 +31,42 @@ public class ThreeSum {
                     set.add(list);
                     j++;
                     k--;
-                }else if (nums[i]+nums[j]+nums[k] > 0) {
+                } else if (nums[i] + nums[j] + nums[k] > 0) {
                     k--;
-                }else {
+                } else {
                     j++;
                 }
             }
         }
         List<List<Integer>> lists = new ArrayList<>(set);
         return lists;
+    }
+
+    // 二刷
+    public List<List<Integer>> threeSum_2(int[] nums) {
+        Arrays.sort(nums);
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    set.add(list);
+                    j++;
+                    k--;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        List<List<Integer>> res = new ArrayList<>(set);
+        return res;
     }
 }
