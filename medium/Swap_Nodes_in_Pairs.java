f2841d1581b98818a501697416ad9eb18639bb18
GJZ0117 <435390736@qq.com>
2023-08-28 11:39:03 +0800

    24. Swap Nodes in Pairs

--- medium/Swap_Nodes_in_Pairs.java
+++ medium/Swap_Nodes_in_Pairs.java
@@ -14,7 +14,7 @@ public class Swap_Nodes_in_Pairs {
         ListNode pre = h;
         ListNode cur = head;
 
-        while (cur != null && cur.next !=null) {
+        while (cur != null && cur.next != null) {
             pre.next = cur.next;
             cur.next = cur.next.next;
             pre.next.next = cur;
@@ -22,5 +22,31 @@ public class Swap_Nodes_in_Pairs {
             cur = cur.next;
         }
         return h.next;
+    }
+
+
+    //二刷
+    public ListNode swapPairs_2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = new ListNode();
+        ListNode cur = newHead;
+        ListNode tempHead;
+        while (head != null && head.next != null) {
+            tempHead = head.next.next;
+            cur.next = head.next;
+            cur = cur.next;
+            cur.next = head;
+            cur = cur.next;
+            cur.next = null;
+            head = tempHead;
+        }
+        if (head != null) {
+            cur.next = head;
+            cur = cur.next;
+            cur.next = null;
+        }
+        return newHead.next;
     }
 }
