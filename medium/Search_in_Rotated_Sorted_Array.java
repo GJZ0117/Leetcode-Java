945eba223cee9b1dfdcd17f10d9f29595cbee8e4
GJZ0117 <435390736@qq.com>
2023-08-29 11:10:40 +0800

    33. Search in Rotated Sorted Array

--- medium/Search_in_Rotated_Sorted_Array.java
+++ medium/Search_in_Rotated_Sorted_Array.java
@@ -7,14 +7,14 @@ package medium;
 public class Search_in_Rotated_Sorted_Array {
 
     public static void main(String[] args) {
-        int[] nums = {1,3,5};
+        int[] nums = {1, 3, 5};
         System.out.println(search(nums, 5));
     }
 
     /*
-    * For those who struggled to figure out why it is realmid=(mid+rot)%n: you can think of it this way:
-    * If we want to find realmid for array [4,5,6,7,0,1,2], you can shift the part before the rotating point to the end of the array (after 2) so that the sorted array is "recovered" from the rotating point but only longer, like this: [4, 5, 6, 7, 0, 1, 2, 4, 5, 6, 7]. The real mid in this longer array is the middle point between the rotating point and the last element: (rot + (hi+rot)) / 2. (hi + rot) is the index of the last element. And of course, this result is larger than the real middle. So you just need to wrap around and get the remainder: ((rot + (hi + rot)) / 2) % n. And this expression is effectively (rot + hi/2) % n, which is (rot+mid) % n.
-    * */
+     * For those who struggled to figure out why it is realmid=(mid+rot)%n: you can think of it this way:
+     * If we want to find realmid for array [4,5,6,7,0,1,2], you can shift the part before the rotating point to the end of the array (after 2) so that the sorted array is "recovered" from the rotating point but only longer, like this: [4, 5, 6, 7, 0, 1, 2, 4, 5, 6, 7]. The real mid in this longer array is the middle point between the rotating point and the last element: (rot + (hi+rot)) / 2. (hi + rot) is the index of the last element. And of course, this result is larger than the real middle. So you just need to wrap around and get the remainder: ((rot + (hi + rot)) / 2) % n. And this expression is effectively (rot + hi/2) % n, which is (rot+mid) % n.
+     * */
 
     public static int search(int[] nums, int target) {
         int low = 0;
@@ -45,4 +45,34 @@ public class Search_in_Rotated_Sorted_Array {
         return -1;
     }
 
+
+    // 二刷(还不会)
+    // https://leetcode.cn/problems/search-in-rotated-sorted-array/solutions/221435/duo-si-lu-wan-quan-gong-lue-bi-xu-miao-dong-by-swe/
+    public int search_2(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (target >= nums[0]) {
+                if (nums[mid] < nums[0]) {
+                    nums[mid] = Integer.MAX_VALUE;
+                }
+            } else {
+                if (nums[mid] >= nums[0]) {
+                    nums[mid] = Integer.MIN_VALUE;
+                }
+            }
+
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
