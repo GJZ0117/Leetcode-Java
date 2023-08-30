ec0517a6b9c8e1b08e09520e83c18cadf4c2e11c
GJZ0117 <435390736@qq.com>
2023-08-30 11:51:12 +0800

    36. Valid Sudoku

--- medium/Valid_Sudoku.java
+++ medium/Valid_Sudoku.java
@@ -1,5 +1,7 @@
 package medium;
 
+import java.util.Arrays;
+
 /**
  * 36. Valid Sudoku
  */
@@ -34,8 +36,8 @@ public class Valid_Sudoku {
             }
         }
         //check 3*3 box
-        int box_i = (i / 3)*3;
-        int box_j = (j / 3)*3;
+        int box_i = (i / 3) * 3;
+        int box_j = (j / 3) * 3;
         for (int x = box_i; x < box_i + 3; x++) {
             for (int y = box_j; y < box_j + 3; y++) {
                 if ((board[x][y] == board[i][j]) && (x != i && y != j)) {
@@ -44,6 +46,79 @@ public class Valid_Sudoku {
                 }
             }
         }
+        return true;
+    }
+
+
+    // 二刷
+    public boolean isValidSudoku_2(char[][] board) {
+        int[] flag = new int[9];
+        for (int i = 0; i < 9; i++) {
+            if (!checkRow(board, i, flag)) {
+                return false;
+            }
+        }
+        for (int j = 0; j < 9; j++) {
+            if (!checkCol(board, j, flag)) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (!checkSubBox(board, i * 3, j * 3, flag)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkRow(char[][] board, int i, int[] flag) {
+        for (int j = 0; j < 9; j++) {
+            char c = board[i][j];
+            if (c == '.') {
+                continue;
+            } else if (flag[c - '1'] != 0) {
+                Arrays.fill(flag, 0);
+                return false;
+            }
+            flag[c - '1']++;
+        }
+        Arrays.fill(flag, 0);
+        return true;
+    }
+
+    private boolean checkCol(char[][] board, int j, int[] flag) {
+        for (int i = 0; i < 9; i++) {
+            char c = board[i][j];
+            if (c == '.') {
+                continue;
+            } else if (flag[c - '1'] != 0) {
+                Arrays.fill(flag, 0);
+                return false;
+            }
+            flag[c - '1']++;
+        }
+        Arrays.fill(flag, 0);
+        return true;
+    }
+
+    private boolean checkSubBox(char[][] board, int i, int j, int[] flag) {
+        for (int x = i; x < i + 3; x++) {
+            for (int y = j; y < j + 3; y++) {
+                char c = board[x][y];
+                if (c == '.') {
+                    continue;
+                } else if (flag[c - '1'] != 0) {
+                    Arrays.fill(flag, 0);
+                    return false;
+                }
+                flag[c - '1']++;
+            }
+        }
+        Arrays.fill(flag, 0);
         return true;
     }
 }
