bf38723805dafa06daafebf846e97395c9233358
GJZ0117 <435390736@qq.com>
2023-09-03 11:16:08 +0800

    47. Permutations II

--- medium/Permutations_II.java
+++ medium/Permutations_II.java
@@ -9,7 +9,7 @@ import java.util.*;
 public class Permutations_II {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        if (nums==null || nums.length == 0) {
+        if (nums == null || nums.length == 0) {
             return ans;
         }
         generate(ans, nums, 0);
@@ -26,10 +26,10 @@ public class Permutations_II {
             return;
         }
         Set<Integer> appeared = new HashSet<>();
-        for (int i=index; i<nums.length; i++) {
+        for (int i = index; i < nums.length; i++) {
             if (appeared.add(nums[i])) {
                 swap(nums, index, i);
-                generate(ans, nums, index+1);
+                generate(ans, nums, index + 1);
                 swap(nums, index, i);
             }
         }
@@ -39,5 +39,41 @@ public class Permutations_II {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+
+
+    // 二刷(还不会)
+    // https://leetcode.cn/problems/permutations-ii/solutions/418230/47-quan-pai-lie-iiche-di-li-jie-pai-lie-zhong-de-q/
+
+    public List<List<Integer>> permuteUnique_2(int[] nums) {
+        boolean[] used = new boolean[nums.length];
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.fill(used, false);
+        Arrays.sort(nums);
+        backTrack(nums, used, res, new ArrayList<>());
+        return res;
+    }
+
+    private void backTrack(int[] nums, boolean[] used, List<List<Integer>> res, List<Integer> path) {
+        if (path.size() == nums.length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            // used[i - 1] == true，说明同⼀树⽀nums[i - 1]使⽤过
+            // used[i - 1] == false，说明同⼀树层nums[i - 1]使⽤过
+            // 如果同⼀树层nums[i - 1]使⽤过则直接跳过
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                continue;
+            }
+            //如果同⼀树⽀nums[i]没使⽤过开始处理
+            if (!used[i]) {
+                used[i] = true;
+                path.add(nums[i]);
+                backTrack(nums, used, res, path);
+                path.remove(path.size() - 1);
+                used[i] = false;
+            }
+        }
     }
 }
