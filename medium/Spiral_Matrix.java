d4337ffb24baefaac6109c531270603aa3731319
GJZ0117 <435390736@qq.com>
2023-09-06 09:40:08 +0800

    54. Spiral Matrix

--- medium/Spiral_Matrix.java
+++ medium/Spiral_Matrix.java
@@ -62,4 +62,54 @@ public class Spiral_Matrix {
         return ans;
     }
 
+
+    // 二刷
+    public List<Integer> spiralOrder_2(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int total = m * n;
+        int i = 0;
+        int j = 0;
+        List<Integer> list = new ArrayList<>(m * n);
+        while (total > 0) {
+            while (j < n && !visited[i][j]) {
+                list.add(matrix[i][j]);
+                visited[i][j] = true;
+                j++;
+                total--;
+            }
+            i++;
+            j--;
+
+            while (i < m && !visited[i][j]) {
+                list.add(matrix[i][j]);
+                visited[i][j] = true;
+                i++;
+                total--;
+            }
+            i--;
+            j--;
+
+            while (j >= 0 && !visited[i][j]) {
+                list.add(matrix[i][j]);
+                visited[i][j] = true;
+                j--;
+                total--;
+            }
+            i--;
+            j++;
+
+            while (i >= 0 && !visited[i][j]) {
+                list.add(matrix[i][j]);
+                visited[i][j] = true;
+                i--;
+                total--;
+            }
+            i++;
+            j++;
+        }
+        return list;
+    }
+
 }
