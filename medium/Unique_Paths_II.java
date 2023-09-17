a56e1b6484cc7ca57a9eacd0dc9b401baa3a1b80
GJZ0117 <435390736@qq.com>
2023-09-17 08:54:21 +0100

    63. Unique Paths II

--- medium/Unique_Paths_II.java
+++ medium/Unique_Paths_II.java
@@ -15,7 +15,7 @@ public class Unique_Paths_II {
         for (int i = 0; i < m; i++) {
             if (!obstacleFlag && obstacleGrid[i][0] == 0) {
                 dp[i][0] = 1;
-            }else {
+            } else {
                 dp[i][0] = 0;
                 obstacleFlag = true;
             }
@@ -23,9 +23,9 @@ public class Unique_Paths_II {
 
         obstacleFlag = false;
         for (int j = 0; j < n; j++) {
-            if (!obstacleFlag && obstacleGrid[0][j] == 0){
+            if (!obstacleFlag && obstacleGrid[0][j] == 0) {
                 dp[0][j] = 1;
-            }else {
+            } else {
                 dp[0][j] = 0;
                 obstacleFlag = true;
             }
@@ -34,12 +34,36 @@ public class Unique_Paths_II {
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (obstacleGrid[i][j] == 0) {
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                } else {
                     dp[i][j] = 0;
                 }
             }
         }
-        return dp[m-1][n-1];
+        return dp[m - 1][n - 1];
+    }
+
+
+    // 二刷
+    public int uniquePathsWithObstacles_2(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n && obstacleGrid[0][j] == 0; j++) {
+            dp[0][j] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 }
