468444e0e8b4b7ff488502ce37c812e5e9311592
GJZ0117 <435390736@qq.com>
2023-09-18 08:19:46 +0100

    64. Minimum Path Sum

--- medium/Minimum_Path_Sum.java
+++ medium/Minimum_Path_Sum.java
@@ -7,7 +7,7 @@ package medium;
 public class Minimum_Path_Sum {
 
     public static void main(String[] args) {
-        int[][] grid = {{1,2,3},{4,5,6}};
+        int[][] grid = {{1, 2, 3}, {4, 5, 6}};
         System.out.println(minPathSum(grid));
     }
 
@@ -25,6 +25,27 @@ public class Minimum_Path_Sum {
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+
+    // 二刷
+    public int minPathSum_2(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
             }
         }
         return dp[m - 1][n - 1];
