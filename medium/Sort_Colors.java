d2a71a622761f3a76d1f303e3062c75a748e6cc2
GJZ0117 <435390736@qq.com>
2023-09-22 11:00:46 +0100

    75. Sort Colors

--- medium/Sort_Colors.java
+++ medium/Sort_Colors.java
@@ -1,19 +1,13 @@
 package medium;
 
-import java.util.Arrays;
 
 /**
  * 75. Sort Colors
  */
 
 public class Sort_Colors {
-    public static void main(String[] args) {
-        int[] nums = {2,0,1};
-        sortColors(nums);
-        System.out.println(Arrays.toString(nums));
-    }
 
-    public static void sortColors(int[] nums) {
+    public void sortColors(int[] nums) {
         int i = 0;
         int j = nums.length - 1;
         for (int cur = 0; cur <= j; ) {
@@ -30,9 +24,29 @@ public class Sort_Colors {
         }
     }
 
-    public static void swap(int[] nums, int a, int b) {
+    public void swap(int[] nums, int a, int b) {
         int temp = nums[a];
         nums[a] = nums[b];
         nums[b] = temp;
+    }
+
+
+    // 二刷(还不会)
+    public void sortColors_2(int[] nums) {
+        int left = 0;
+        int mid = 0;
+        int right = nums.length - 1;
+        while (mid <= right) {
+            if (nums[mid] == 0) {
+                swap(nums, left, mid);
+                left++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums, mid, right);
+                right--;
+            }
+        }
     }
 }
