4f649f162d94762c1806d9b67c42b577c743c9e3
GJZ0117 <435390736@qq.com>
2023-09-27 20:18:38 +0100

    81. Search in Rotated Sorted Array II

--- medium/Search_in_Rotated_Sorted_Array_II.java
+++ medium/Search_in_Rotated_Sorted_Array_II.java
@@ -31,4 +31,41 @@ public class Search_in_Rotated_Sorted_Array_II {
         }
         return false;
     }
+
+
+    // 二刷(还不会)
+    public boolean search_2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            while (left < right && nums[left] == nums[left + 1]) {
+                left++;
+            }
+            while (left < right && nums[right] == nums[right - 1]) {
+                right--;
+            }
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[mid] >= target && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
 }
