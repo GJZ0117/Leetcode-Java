450dd10c732f89d15d500f4306cb2e41e2c4e682
GJZ0117 <435390736@qq.com>
2023-10-01 08:19:14 +0100

    91. Decode Ways

--- medium/Decode_Ways.java
+++ medium/Decode_Ways.java
@@ -25,4 +25,28 @@ public class Decode_Ways {
         }
         return dp[n];
     }
+
+    // 二刷（还不会）
+    // dp[0] means an empty string will have one way to decode, dp[1] means the way to decode a string of size 1
+    // then check one digit and two digit combination and save the results along the way. In the end, dp[n] will be the end result
+    public int numDecodings_2(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for (int i = 2; i <= n; i++) {
+            int first = Integer.parseInt(s.substring(i - 1, i));
+            int second = Integer.parseInt(s.substring(i - 2, i));
+            if (first >= 1 && first <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
 }
