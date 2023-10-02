d546e8f75b486f65bd5c652807f46481e82569e6
GJZ0117 <435390736@qq.com>
2023-10-02 08:03:24 +0100

    92. Reverse Linked List II

--- medium/Reverse_Linked_List_II.java
+++ medium/Reverse_Linked_List_II.java
@@ -35,4 +35,41 @@ public class Reverse_Linked_List_II {
         }
         return newHead.next;
     }
+
+
+    // 二刷
+    public ListNode reverseBetween_2(ListNode head, int left, int right) {
+        if (left == right) {
+            return head;
+        }
+        ListNode newHead = new ListNode();
+        newHead.next = head;
+        int count = right - left;
+        ListNode cur = newHead;
+        ListNode start = null;
+        ListNode end = null;
+        ListNode first = null;
+        for (int i = 1; i < left; i++) {
+            cur = cur.next;
+        }
+        start = cur;
+        for (int i = 0; i <= count; i++) {
+            cur = cur.next;
+        }
+        end = cur.next;
+        cur = start.next;
+        start.next = null;
+        ListNode temp;
+        for (int i = 0; i <= count; i++) {
+            if (first == null) {
+                first = cur;
+            }
+            temp = cur.next;
+            cur.next = start.next;
+            start.next = cur;
+            cur = temp;
+        }
+        first.next = end;
+        return newHead.next;
+    }
 }
