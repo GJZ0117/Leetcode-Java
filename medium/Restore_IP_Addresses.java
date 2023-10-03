387b8f24be8f7f3ddc96e350587606548a5b38f5
GJZ0117 <435390736@qq.com>
2023-10-03 09:12:50 +0100

    93. Restore IP Addresses

--- medium/Restore_IP_Addresses.java
+++ medium/Restore_IP_Addresses.java
@@ -42,4 +42,29 @@ public class Restore_IP_Addresses {
             sb.deleteCharAt(i);
         }
     }
+
+
+    // 二刷（还不会）
+    public List<String> restoreIpAddresse_2(String s) {
+        List<String> list = new ArrayList<>();
+        int n = s.length();
+        for (int i = 0; i < 4 && i < n; i++) {
+            for (int j = i + 1; j < i + 4 && j < n; j++) {
+                for (int k = j + 1; k < j + 4 && k < n; k++) {
+                    String segment1 = s.substring(0, i);
+                    String segment2 = s.substring(i, j);
+                    String segment3 = s.substring(j, k);
+                    String segment4 = s.substring(k);
+                    if (check(segment1) && check(segment2) && check(segment3) && check(segment4)) {
+                        list.add(segment1 + "." + segment2 + "." + segment3 + "." + segment4);
+                    }
+                }
+            }
+        }
+        return list;
+    }
+
+    private boolean check(String s) {
+        return s.length() <= 3 && s.length() != 0 && (s.charAt(0) != '0' || s.length() <= 1) && Integer.parseInt(s) <= 255;
+    }
 }
