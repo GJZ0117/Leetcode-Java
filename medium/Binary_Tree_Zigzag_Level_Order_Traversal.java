4cd123733828c08ddcbb7142b9e12723e02549ef
GJZ0117 <435390736@qq.com>
2023-10-12 07:25:52 +0100

    103. Binary Tree Zigzag Level Order Traversal

--- medium/Binary_Tree_Zigzag_Level_Order_Traversal.java
+++ medium/Binary_Tree_Zigzag_Level_Order_Traversal.java
@@ -55,4 +55,46 @@ public class Binary_Tree_Zigzag_Level_Order_Traversal {
         }
         return ans;
     }
+
+
+    // 二刷
+    public List<List<Integer>> zigzagLevelOrder_2(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Deque<TreeNode> deque = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        boolean leftToRight = true;
+        int len = 0;
+        TreeNode p;
+        deque.addLast(root);
+        while (!deque.isEmpty()) {
+            len = deque.size();
+            while (len > 0) {
+                p = leftToRight ? deque.removeFirst() : deque.removeLast();
+                list.add(p.val);
+                if (leftToRight) {
+                    if (p.left != null) {
+                        deque.addLast(p.left);
+                    }
+                    if (p.right != null) {
+                        deque.addLast(p.right);
+                    }
+                } else {
+                    if (p.right != null) {
+                        deque.addFirst(p.right);
+                    }
+                    if (p.left != null) {
+                        deque.addFirst(p.left);
+                    }
+                }
+                len--;
+            }
+            res.add(new ArrayList<>(list));
+            list.clear();
+            leftToRight = !leftToRight;
+        }
+        return res;
+    }
 }
