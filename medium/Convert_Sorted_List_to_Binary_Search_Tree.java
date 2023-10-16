1d409a6cf00b17f92ebfc617a4e4f18a8bf9f021
GJZ0117 <435390736@qq.com>
2023-10-16 08:02:58 +0100

    109. Convert Sorted List to Binary Search Tree

--- medium/Convert_Sorted_List_to_Binary_Search_Tree.java
+++ medium/Convert_Sorted_List_to_Binary_Search_Tree.java
@@ -27,4 +27,27 @@ public class Convert_Sorted_List_to_Binary_Search_Tree {
         root.right = generate(slow.next, tail);
         return root;
     }
+
+
+    // 二刷（还不会）
+    public TreeNode sortedListToBST_2(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return new TreeNode(head.val);
+        }
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        TreeNode root = new TreeNode(slow.next.val);
+        ListNode rightPartStart = slow.next.next;
+        slow.next = null;
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(rightPartStart);
+        return root;
+    }
 }
