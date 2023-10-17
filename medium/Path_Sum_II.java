639f3ee0e5a087064c27f1855cd7aeda35d1b54e
GJZ0117 <435390736@qq.com>
2023-10-17 08:50:03 +0100

    113. Path Sum II

--- medium/Path_Sum_II.java
+++ medium/Path_Sum_II.java
@@ -39,4 +39,35 @@ public class Path_Sum_II {
         list.remove(list.size() - 1);
     }
 
+
+    public List<List<Integer>> pathSum_2(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        preOrder(root, targetSum, 0, res, new ArrayList<>());
+        return res;
+    }
+
+    private void preOrder(TreeNode node, int targetSum, int curSum, List<List<Integer>> res, List<Integer> list) {
+        list.add(node.val);
+
+        if (node.left == null && node.right == null) {
+            if (curSum + node.val == targetSum) {
+                res.add(new ArrayList<>(list));
+            }
+            list.remove(list.size() - 1);
+            return;
+        }
+
+        if (node.left != null) {
+            preOrder(node.left, targetSum, curSum + node.val, res, list);
+        }
+
+        if (node.right != null) {
+            preOrder(node.right, targetSum, curSum + node.val, res, list);
+        }
+
+        list.remove(list.size() - 1);
+    }
 }
