57309a38708e150cfc1522e1bf69000e49fa2816
GJZ0117 <435390736@qq.com>
2023-10-20 08:26:50 +0100

    117. Populating Next Right Pointers in Each Node II

--- medium/Populating_Next_Right_Pointers_in_Each_Node_II.java
+++ medium/Populating_Next_Right_Pointers_in_Each_Node_II.java
@@ -1,5 +1,8 @@
 package medium;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * 117. Populating Next Right Pointers in Each Node II
  */
@@ -27,6 +30,37 @@ public class Populating_Next_Right_Pointers_in_Each_Node_II {
                 cur = cur.next;
             }
             cur = newHead.next;
+        }
+        return root;
+    }
+
+
+    // 二刷
+    public Node connect_2(Node root) {
+        if (root == null) {
+            return root;
+        }
+        Deque<Node> deque = new ArrayDeque<>();
+        Node p;
+        int len;
+        deque.addLast(root);
+        while (!deque.isEmpty()) {
+            len = deque.size();
+            while (len > 0) {
+                len--;
+                p = deque.removeFirst();
+                if (len == 0) {
+                    p.next = null;
+                } else {
+                    p.next = deque.peekFirst();
+                }
+                if (p.left != null) {
+                    deque.addLast(p.left);
+                }
+                if (p.right != null) {
+                    deque.addLast(p.right);
+                }
+            }
         }
         return root;
     }
