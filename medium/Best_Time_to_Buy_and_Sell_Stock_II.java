a42811750c150b118f574a1626ef9f0024d5a733
GJZ0117 <435390736@qq.com>
2023-10-22 08:04:00 +0100

    122. Best Time to Buy and Sell Stock II

--- medium/Best_Time_to_Buy_and_Sell_Stock_II.java
+++ medium/Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -7,7 +7,7 @@ package medium;
 public class Best_Time_to_Buy_and_Sell_Stock_II {
 
     public static void main(String[] args) {
-        int[] prices = {7,6,4,3,1};
+        int[] prices = {7, 6, 4, 3, 1};
         System.out.println(maxProfit(prices));
     }
 
@@ -25,5 +25,17 @@ public class Best_Time_to_Buy_and_Sell_Stock_II {
             pre = cur; // we can buy the stock at the same day when we sold it, or if previous value is higher than current value, we can buy it today
         }
         return maxProfit;
+    }
+
+
+    // 二刷（还不会）
+    public int maxProfit_2(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
     }
 }
