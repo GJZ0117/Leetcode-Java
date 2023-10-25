96d9ad3a5f6f5a63656c06e8248cdf215de2fab7
GJZ0117 <435390736@qq.com>
2023-10-25 08:10:50 +0100

    130. Surrounded Regions

--- medium/Surrounded_Regions.java
+++ medium/Surrounded_Regions.java
@@ -1,5 +1,8 @@
 package medium;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 130. Surrounded Regions
  */
@@ -59,4 +62,60 @@ public class Surrounded_Regions {
         }
     }
 
+
+    // 二刷
+
+    List<int[]> edgeOPos = new ArrayList<>();
+
+    public void solve_2(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                convertEdgeOtoZ(board, i, 0);
+            }
+            if (board[i][n - 1] == 'O') {
+                convertEdgeOtoZ(board, i, n - 1);
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') {
+                convertEdgeOtoZ(board, 0, j);
+            }
+            if (board[m - 1][j] == 'O') {
+                convertEdgeOtoZ(board, m - 1, j);
+            }
+        }
+
+        for (int i = 1; i < m - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+
+        for (int[] pos : edgeOPos) {
+            board[pos[0]][pos[1]] = 'O';
+        }
+    }
+
+    private void convertEdgeOtoZ(char[][] board, int x, int y) {
+        board[x][y] = 'Z';
+        edgeOPos.add(new int[]{x, y});
+        if (x - 1 >= 0 && board[x - 1][y] == 'O') {
+            convertEdgeOtoZ(board, x - 1, y);
+        }
+        if (x + 1 < board.length && board[x + 1][y] == 'O') {
+            convertEdgeOtoZ(board, x + 1, y);
+        }
+        if (y - 1 >= 0 && board[x][y - 1] == 'O') {
+            convertEdgeOtoZ(board, x, y - 1);
+        }
+        if (y + 1 < board[0].length && board[x][y + 1] == 'O') {
+            convertEdgeOtoZ(board, x, y + 1);
+        }
+    }
 }
