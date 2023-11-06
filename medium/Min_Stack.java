e74e11266856be8fb8cc994f7363e829b186c30b
GJZ0117 <435390736@qq.com>
2023-11-06 08:13:08 +0000

    155. Min Stack

--- medium/Min_Stack.java
+++ medium/Min_Stack.java
@@ -1,6 +1,9 @@
 package medium;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
+import java.util.TreeMap;
 
 /**
  * 155. Min Stack
@@ -36,3 +39,35 @@ public class Min_Stack {
         return minStack.peek();
     }
 }
+
+class MinStack {
+
+    Deque<Integer> deque = new ArrayDeque<>();
+    TreeMap<Integer, Integer> map = new TreeMap<>();
+
+    public MinStack() {
+    }
+
+    public void push(int val) {
+        deque.addLast(val);
+        map.put(val, map.getOrDefault(val, 0) + 1);
+    }
+
+    public void pop() {
+        int key = deque.removeLast();
+        if (map.get(key).equals(1)) {
+            map.remove(key);
+        } else {
+            map.put(key, map.get(key) - 1);
+        }
+    }
+
+    public int top() {
+        return deque.peekLast();
+    }
+
+    public int getMin() {
+        return map.firstKey();
+    }
+}
+
