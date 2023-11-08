27a05bb64d5b2afcc55b717f2301073c85f0c833
GJZ0117 <435390736@qq.com>
2023-11-08 07:49:59 +0000

    165. Compare Version Numbers

--- medium/Compare_Version_Numbers.java
+++ medium/Compare_Version_Numbers.java
@@ -42,4 +42,37 @@ public class Compare_Version_Numbers {
 
         return 0;
     }
+
+
+    // 二刷
+    public int compareVersion_2(String version1, String version2) {
+        String[] strs1 = version1.split("\\.");
+        String[] strs2 = version2.split("\\.");
+        int minLen = Math.min(strs1.length, strs2.length);
+        for (int i = 0; i < minLen; i++) {
+            int n1 = Integer.parseInt(strs1[i]);
+            int n2 = Integer.parseInt(strs2[i]);
+            if (n1 > n2) {
+                return 1;
+            } else if (n2 > n1) {
+                return -1;
+            }
+        }
+
+        if (strs1.length > minLen) {
+            for (int i = minLen; i < strs1.length; i++) {
+                if (Integer.parseInt(strs1[i]) > 0) {
+                    return 1;
+                }
+            }
+        } else if (strs2.length > minLen) {
+            for (int i = minLen; i < strs2.length; i++) {
+                if (Integer.parseInt(strs2[i]) > 0) {
+                    return -1;
+                }
+            }
+        }
+
+        return 0;
+    }
 }
