638a3c2dbc1ea2ec0dbcf59f82213a26083f251a
GJZ0117 <435390736@qq.com>
2023-11-11 08:05:38 +0000

    167. Two Sum II - Input Array Is Sorted

--- medium/Two_Sum_II_Input_Array_Is_Sorted.java
+++ medium/Two_Sum_II_Input_Array_Is_Sorted.java
@@ -22,4 +22,25 @@ public class Two_Sum_II_Input_Array_Is_Sorted {
         }
         return ans;
     }
+
+
+    // 二刷
+    public int[] twoSum_2(int[] numbers, int target) {
+        int[] res = new int[2];
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                res[0] = left + 1;
+                res[1] = right + 1;
+                return res;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return res;
+    }
 }
