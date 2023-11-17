8b8d03bb509437d8604fbc39f9cac8813df211b7
GJZ0117 <435390736@qq.com>
2023-11-17 08:53:50 +0000

    199. Binary Tree Right Side View

--- medium/Binary_Tree_Right_Side_View.java
+++ medium/Binary_Tree_Right_Side_View.java
@@ -1,9 +1,6 @@
 package medium;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * 199. Binary Tree Right Side View
@@ -34,5 +31,33 @@ public class Binary_Tree_Right_Side_View {
             }
         }
         return list;
+    }
+
+
+    // 二刷
+    public List<Integer> rightSideView_2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        TreeNode p;
+        deque.addLast(root);
+        while (!deque.isEmpty()) {
+            int len = deque.size();
+            for (int i = 0; i < len; i++) {
+                p = deque.removeFirst();
+                if (i == 0) {
+                    res.add(p.val);
+                }
+                if (p.right != null) {
+                    deque.add(p.right);
+                }
+                if (p.left != null) {
+                    deque.add(p.left);
+                }
+            }
+        }
+        return res;
     }
 }
