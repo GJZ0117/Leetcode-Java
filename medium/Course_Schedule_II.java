240034f5977bb91d064baaebf4a99f21e207cc7e
GJZ0117 <435390736@qq.com>
2023-11-23 09:41:43 +0000

    210. Course Schedule II

--- medium/Course_Schedule_II.java
+++ medium/Course_Schedule_II.java
@@ -10,7 +10,7 @@ public class Course_Schedule_II {
 
     public static void main(String[] args) {
         int numCourses = 2;
-        int[][] prerequisites = {{1,0}};
+        int[][] prerequisites = {{1, 0}};
         System.out.println(Arrays.toString(findOrder(numCourses, prerequisites)));
     }
 
@@ -58,6 +58,52 @@ public class Course_Schedule_II {
     public static int findZero(int[] array, boolean[] visited, Map<Integer, List<Integer>> map) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == 0 && !visited[i]) {
+                visited[i] = true;
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    // 二刷
+    public int[] findOrder_2(int numCourses, int[][] prerequisites) {
+        int[] res = new int[numCourses];
+        int resIndex = 0;
+        int[] cnt = new int[numCourses];
+        boolean[] visited = new boolean[numCourses];
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int[] arr : prerequisites) {
+            int cur = arr[0];
+            int pre = arr[1];
+            cnt[cur]++;
+            if (map.containsKey(pre)) {
+                map.get(pre).add(cur);
+            } else {
+                List<Integer> list = new ArrayList<>();
+                list.add(cur);
+                map.put(pre, list);
+            }
+        }
+
+        while (true) {
+            int index = findZeroPreIndex(cnt, visited);
+            if (index == -1) {
+                break;
+            }
+            res[resIndex++] = index;
+            if (map.containsKey(index)) {
+                for (int i : map.get(index)) {
+                    cnt[i]--;
+                }
+            }
+        }
+        return resIndex == numCourses ? res : new int[0];
+    }
+
+    private int findZeroPreIndex(int[] cnt, boolean[] visited) {
+        for (int i = 0; i < cnt.length; i++) {
+            if (cnt[i] == 0 && !visited[i]) {
                 visited[i] = true;
                 return i;
             }
