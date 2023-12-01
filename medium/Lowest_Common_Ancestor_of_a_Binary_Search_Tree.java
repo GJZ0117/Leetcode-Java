f51dfb9e3bc65f291062915f3230b69ec335e49c
GJZ0117 <435390736@qq.com>
2023-12-01 08:38:59 +0000

    235. Lowest Common Ancestor of a Binary Search Tree

--- medium/Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
+++ medium/Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -8,9 +8,20 @@ public class Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root.val > p.val && root.val > q.val) {
             return lowestCommonAncestor(root.left, p, q);
-        }else if (root.val < p.val && root.val < q.val) {
+        } else if (root.val < p.val && root.val < q.val) {
             return lowestCommonAncestor(root.right, p, q);
-        }else {
+        } else {
+            return root;
+        }
+    }
+
+    // 二刷(用栈记录所有父节点方法太复杂)
+    public TreeNode lowestCommonAncestor_2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor_2(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor_2(root.right, p, q);
+        } else {
             return root;
         }
     }
