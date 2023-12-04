34f744fbe2d602995802be6aaddd23987e1b82b4
GJZ0117 <435390736@qq.com>
2023-12-04 07:53:14 +0000

    238. Product of Array Except Self

--- medium/Product_of_Array_Except_Self.java
+++ medium/Product_of_Array_Except_Self.java
@@ -23,4 +23,20 @@ public class Product_of_Array_Except_Self {
         }
         return res;
     }
+
+
+    // 二刷（还不会）
+    public int[] productExceptSelf_2(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] = res[i] * right;
+            right = right * nums[i];
+        }
+        return res;
+    }
 }
