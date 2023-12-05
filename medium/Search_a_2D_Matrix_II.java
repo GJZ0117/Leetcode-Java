1aea4b724b794c5ef2bc1a9735d734f94ab8060e
GJZ0117 <435390736@qq.com>
2023-12-05 09:02:20 +0000

    240. Search a 2D Matrix II

--- medium/Search_a_2D_Matrix_II.java
+++ medium/Search_a_2D_Matrix_II.java
@@ -37,4 +37,25 @@ public class Search_a_2D_Matrix_II {
         }
         return false;
     }
+
+
+    // 二刷
+    public boolean searchMatrix_2(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] <= target && matrix[i][n - 1] >= target) {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][j] == target) {
+                        return true;
+                    } else if (matrix[i][j] > target) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
 }
