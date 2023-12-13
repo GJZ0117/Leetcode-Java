cf76b414c980cddb46820b1d54ee9ef00c06cee5
GJZ0117 <435390736@qq.com>
2023-12-13 21:52:59 +0000

    1051. Height Checker

---
+++ easy/Height_Checker.java
@@ -0,0 +1,21 @@
+package easy;
+
+import java.util.Arrays;
+
+/**
+ * 1051. Height Checker
+ */
+
+public class Height_Checker {
+    public int heightChecker(int[] heights) {
+        int[] expected = Arrays.copyOf(heights, heights.length);
+        Arrays.sort(expected);
+        int count = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != expected[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
