8700bb9121a395c5112ca595e6bbb7cc7bfd604c
GJZ0117 <435390736@qq.com>
2023-12-24 09:31:50 +0000

    275. H-Index II

--- medium/H_Index_II.java
+++ medium/H_Index_II.java
@@ -18,4 +18,21 @@ public class H_Index_II {
         }
         return citations.length - left;
     }
+
+
+    // 二刷
+    public int hIndex_2(int[] citations) {
+        int n = citations.length;
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (citations[mid] >= n - mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return n - left;
+    }
 }
