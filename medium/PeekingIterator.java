8d3ecd8ab3794375f5e8a16065768c94927c589e
GJZ0117 <435390736@qq.com>
2023-12-26 08:57:16 +0000

    284. Peeking Iterator

--- medium/PeekingIterator.java
+++ medium/PeekingIterator.java
@@ -1,6 +1,8 @@
 package medium;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * 284. Peeking Iterator
@@ -35,5 +37,39 @@ class PeekingIterator implements Iterator<Integer> {
     @Override
     public boolean hasNext() {
         return next != null;
+    }
+}
+
+
+// 二刷
+class PeekingIterator_2 implements Iterator<Integer> {
+
+    int index;
+    List<Integer> list;
+
+    public PeekingIterator_2(Iterator<Integer> iterator) {
+        // initialize any member here.
+        index = -1;
+        list = new ArrayList<>();
+        while (iterator.hasNext()) {
+            list.add(iterator.next());
+        }
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        return list.get(index + 1);
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        return list.get(++index);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index != list.size() - 1;
     }
 }
