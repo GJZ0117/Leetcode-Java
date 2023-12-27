9623869b415aa4733387e6c976f14e3a8db91273
GJZ0117 <435390736@qq.com>
2023-12-27 08:34:21 +0000

    287. Find the Duplicate Number

--- medium/Find_the_Duplicate_Number.java
+++ medium/Find_the_Duplicate_Number.java
@@ -7,10 +7,10 @@ package medium;
 public class Find_the_Duplicate_Number {
     /**
      * eg:  4 -> 3 -> 1 -> 2 -> 2
-     *            __________
-     *           |         |
-     *           V         |
-     *      4 -> 2 -> 1 -> 3
+     * __________
+     * |         |
+     * V         |
+     * 4 -> 2 -> 1 -> 3
      */
     public int findDuplicate(int[] nums) {
         int slow = 0;
@@ -29,5 +29,23 @@ public class Find_the_Duplicate_Number {
                 }
             }
         }
+    }
+
+
+    // 二刷（还不会）
+    // https://leetcode.cn/problems/find-the-duplicate-number/solutions/58841/287xun-zhao-zhong-fu-shu-by-kirsche/
+    public int findDuplicate_2(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
 }
