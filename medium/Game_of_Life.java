6d4058dbf55a451f049337ca6a2269228b50dcdf
GJZ0117 <435390736@qq.com>
2023-12-28 09:29:44 +0000

    289. Game of Life

--- medium/Game_of_Life.java
+++ medium/Game_of_Life.java
@@ -10,7 +10,7 @@ public class Game_of_Life {
      * rule 1: previous live, now die : -1
      * rule 2: previous live, now live: 1
      * rule 3: previous live, now die : -1
-     * rule 4: previous die, noe live : 2
+     * rule 4: previous die, now live : 2
      */
 
     public void gameOfLife(int[][] board) {
@@ -53,5 +53,55 @@ public class Game_of_Life {
             }
         }
         return liveCells;
+    }
+
+
+    // 二刷
+    public void gameOfLife_2(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int liveNeighbourCount = liveNeighbour(board, i, j, m, n);
+                if (board[i][j] == 1 && (liveNeighbourCount < 2 || liveNeighbourCount > 3)) {
+                    board[i][j] = -1;
+                } else if (board[i][j] == 1 && (liveNeighbourCount == 2 || liveNeighbourCount == 3)) {
+                    board[i][j] = 1;
+                } else if (board[i][j] == 0 && liveNeighbourCount == 3) {
+                    board[i][j] = 2;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == -1) {
+                    board[i][j] = 0;
+                } else if (board[i][j] == 2) {
+                    board[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    private int liveNeighbour(int[][] board, int i, int j, int m, int n) {
+        int liveNeighbourCount = 0;
+        int[] offset = {-1, 0, 1};
+        for (int xOffset : offset) {
+            for (int yOffset : offset) {
+                if (xOffset == 0 && yOffset == 0) {
+                    continue;
+                }
+                int x = i + xOffset;
+                int y = j + yOffset;
+                if (x < 0 || x >= m || y < 0 || y >=n) {
+                    continue;
+                }
+                if (board[x][y] == 1 || board[x][y] == -1) {
+                    liveNeighbourCount++;
+                }
+            }
+        }
+        return liveNeighbourCount;
     }
 }
