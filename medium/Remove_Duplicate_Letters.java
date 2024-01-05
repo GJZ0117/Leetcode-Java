4d25c6432e66ea0ddfed5db5e2a8f95b99412017
GJZ0117 <435390736@qq.com>
2024-01-05 09:15:35 +0000

    316. Remove Duplicate Letters

--- medium/Remove_Duplicate_Letters.java
+++ medium/Remove_Duplicate_Letters.java
@@ -1,5 +1,7 @@
 package medium;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 /**
@@ -35,5 +37,39 @@ public class Remove_Duplicate_Letters {
             sb.append(stack.pop());
         }
         return sb.reverse().toString();
+    }
+
+
+    // 二刷（还不会）
+    // https://leetcode.cn/problems/remove-duplicate-letters/solutions/528684/yi-kan-jiu-hui-jiu-chai-shou-ba-shou-jia-miqw/
+    public String removeDuplicateLetters_2(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        int[] count = new int[26];
+        boolean[] exist = new boolean[26];
+        // 初始化字母出现次数哈希表
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // 该位置字母没有在栈中出现过
+            if (!exist[c - 'a']) {
+                // 栈不为空 && 栈顶元素字典序列靠后 && 栈顶元素还有剩余出现次数
+                while (!stack.isEmpty() && stack.peekLast() > c && count[stack.peekLast() - 'a'] > 0) {
+                    // 栈顶出栈 修改exist
+                    exist[stack.peekLast() - 'a'] = false;
+                    stack.pollLast();
+                }
+                // 该字符入栈 修改exist
+                exist[c - 'a'] = true;
+                stack.addLast(c);
+            }
+            count[c - 'a']--;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char c : stack) {
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
