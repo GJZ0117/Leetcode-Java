e162530fe47fd26d5c3c6f360356181aa611b00e
GJZ0117 <435390736@qq.com>
2024-01-06 09:41:43 +0000

    318. Maximum Product of Word Lengths

--- medium/Maximum_Product_of_Word_Lengths.java
+++ medium/Maximum_Product_of_Word_Lengths.java
@@ -11,7 +11,7 @@ public class Maximum_Product_of_Word_Lengths {
         System.out.println(maxProduct(words));
     }
 
-    //https://leetcode.cn/problems/maximum-product-of-word-lengths/solution/tong-ge-lai-shua-ti-la-zhao-dao-ti-mu-de-y37h/
+    // https://leetcode.cn/problems/maximum-product-of-word-lengths/solution/tong-ge-lai-shua-ti-la-zhao-dao-ti-mu-de-y37h/
     public static int maxProduct(String[] words) {
         int ans = 0;
         int[] nums = new int[words.length];
@@ -28,5 +28,29 @@ public class Maximum_Product_of_Word_Lengths {
             }
         }
         return ans;
+    }
+
+
+    // 二刷（还不会）
+    public int maxProduct_2(String[] words) {
+        int n = words.length;
+        int index = 0;
+        int max = 0;
+        int[] flag = new int[n];
+        for (String word : words) {
+            int temp = 0;
+            for (char c : word.toCharArray()) {
+                temp |= (1 << c);
+            }
+            flag[index++] = temp;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if ((flag[i] & flag[j]) == 0) {
+                    max = Math.max(max, words[i].length() * words[j].length());
+                }
+            }
+        }
+        return max;
     }
 }
