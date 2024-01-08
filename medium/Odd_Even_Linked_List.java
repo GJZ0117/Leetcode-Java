2bdf005402eb6a222f2250819b1d56b3c0396f3d
GJZ0117 <435390736@qq.com>
2024-01-08 09:27:02 +0000

    328. Odd Even Linked List

--- medium/Odd_Even_Linked_List.java
+++ medium/Odd_Even_Linked_List.java
@@ -27,4 +27,29 @@ public class Odd_Even_Linked_List {
         oddHeadPointer.next = evenHead.next;
         return oddHead.next;
     }
+
+
+    // 二刷
+    public ListNode oddEvenList_2(ListNode head) {
+        ListNode l1 = new ListNode();
+        ListNode l2 = new ListNode();
+        ListNode cur = head;
+        ListNode cur1 = l1;
+        ListNode cur2 = l2;
+        int count = 1;
+        while (cur != null) {
+            if (count % 2 == 1) {
+                cur1.next = cur;
+                cur1 = cur1.next;
+            } else {
+                cur2.next = cur;
+                cur2 = cur2.next;
+            }
+            count++;
+            cur = cur.next;
+            cur2.next = null;
+        }
+        cur1.next = l2.next;
+        return l1.next;
+    }
 }
