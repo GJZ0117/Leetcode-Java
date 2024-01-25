b03c263ff23b7bec674e63a14e910e995eff692c
GJZ0117 <435390736@qq.com>
2024-01-25 12:31:27 +0000

    373. Find K Pairs with Smallest Sums

--- medium/Find_K_Pairs_with_Smallest_Sums.java
+++ medium/Find_K_Pairs_with_Smallest_Sums.java
@@ -21,17 +21,17 @@ public class Find_K_Pairs_with_Smallest_Sums {
     /**
      * eg: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
      * [index1, index2] means nums1's index and nums2's index
-     *
-     *  [0, 0] -> [0, 1] -> [0, 2]
-     *    |
-     *    V
-     *  [1, 0] -> [1, 1] -> [1, 2]
-     *    |
-     *    V
-     *  [2, 0] -> [2, 1] -> [2, 2]
+     * <p>
+     * [0, 0] -> [0, 1] -> [0, 2]
+     * |
+     * V
+     * [1, 0] -> [1, 1] -> [1, 2]
+     * |
+     * V
+     * [2, 0] -> [2, 1] -> [2, 2]
      */
 
-    //https://leetcode.cn/problems/find-k-pairs-with-smallest-sums/solution/tong-ge-lai-shua-ti-la-you-xian-ji-dui-l-fw7y/
+    // https://leetcode.cn/problems/find-k-pairs-with-smallest-sums/solution/tong-ge-lai-shua-ti-la-you-xian-ji-dui-l-fw7y/
     public static List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> nums1[a[0]] + nums2[a[1]] - (nums1[b[0]] + nums2[b[1]]));
         // put all nums1's indexes into heap, nums2's indexes are all 0 (to optimize at most put k times [index1, 0])
@@ -52,7 +52,7 @@ public class Find_K_Pairs_with_Smallest_Sums {
         return ans;
     }
 
-    //Time Limit Exceeded
+    // Time Limit Exceeded
     public static List<List<Integer>> _kSmallestPairs(int[] nums1, int[] nums2, int k) {
         Map<Integer, List<List<Integer>>> map = new HashMap<>();
         for (int i : nums1) {
@@ -90,6 +90,31 @@ public class Find_K_Pairs_with_Smallest_Sums {
                 ans.add(list);
                 k--;
             }
+        }
+        return ans;
+    }
+
+
+    // 二刷(还不会)
+    public List<List<Integer>> kSmallestPairs_2(int[] nums1, int[] nums2, int k) {
+        // 优先级队列 保存 [index1,index2]
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> nums1[a[0]] + nums2[a[1]] - (nums1[b[0]] + nums2[b[1]]));
+        // 把nums1的所有索引入队 nums2索引初始为0
+        for (int i = 0; i < Math.min(nums1.length, k); i++) {
+            heap.add(new int[]{i, 0});
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+
+        // 最多弹出k次
+        while (k > 0 && !heap.isEmpty()) {
+            int[] pos = heap.poll();
+            ans.add(Arrays.asList(nums1[pos[0]], nums2[pos[1]]));
+            // 将 index2加1后入队
+            if (pos[1] + 1 < nums2.length) {
+                pos[1]++;
+                heap.add(pos);
+            }
+            k--;
         }
         return ans;
     }
