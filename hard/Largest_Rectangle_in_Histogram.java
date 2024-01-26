4747691bbbcd0e40e0ae279d87fcd21dd9c2204f
GJZ0117 <435390736@qq.com>
2024-01-26 08:35:28 +0000

    Largest Rectangle in Histogram

--- hard/Largest_Rectangle_in_Histogram.java
+++ hard/Largest_Rectangle_in_Histogram.java
@@ -22,9 +22,9 @@ public class Largest_Rectangle_in_Histogram {
      * 如果栈不为空且当前考察的新元素值比栈顶元素值小，表明以栈顶元素值为高的矩形的面积是可以确定的了。该矩形的高就是栈顶元素值，其右侧边界就是当前考察的新元素，左侧边界是栈顶元素的前一个元素，因为，在上一步中我们知道栈中元素值从栈底到栈顶是依次递增的。 因此，矩形的宽是当前考察的元素索引与栈顶元素前一个元素的索引的差值减一。=
      */
     public static int betterSolution(int[] heights) {
-        int res = 0; //初始化结果为0
-        Stack<Integer> stack = new Stack<>(); //递增栈
-        //将给定的原数组左右各添加一个0
+        int res = 0; // 初始化结果为0
+        Stack<Integer> stack = new Stack<>(); // 递增栈
+        // 将给定的原数组左右各添加一个0
         int[] newHeights = new int[heights.length + 2];
         newHeights[0] = 0;
         newHeights[newHeights.length - 1] = 0;
@@ -33,21 +33,21 @@ public class Largest_Rectangle_in_Histogram {
         }
 
         for (int i = 0; i < newHeights.length; i++) {
-            //栈不空且当前元素值小于栈顶元素值，则表示以栈顶元素为高的矩形面积可以确定
+            // 栈不空且当前元素值小于栈顶元素值，则表示以栈顶元素为高的矩形面积可以确定
             while (!stack.isEmpty() && newHeights[i] < newHeights[stack.peek()]) {
-                int cur = stack.pop(); //弹出栈顶元素
-                int curHeight = newHeights[cur]; //获取栈顶元素对应高
-                int leftIndex = stack.peek(); //栈顶元素弹出后，新的栈顶元素是其左边界
-                int rightIndex = i; //右边界是当前考察的索引
-                int curWidth = rightIndex - leftIndex - 1; //计算矩形宽度
-                res = Math.max(res, curWidth * curHeight); //计算面积
+                int cur = stack.pop(); // 弹出栈顶元素
+                int curHeight = newHeights[cur]; // 获取栈顶元素对应高
+                int leftIndex = stack.peek(); // 栈顶元素弹出后，新的栈顶元素是其左边界
+                int rightIndex = i; // 右边界是当前考察的索引
+                int curWidth = rightIndex - leftIndex - 1; // 计算矩形宽度
+                res = Math.max(res, curWidth * curHeight); // 计算面积
             }
-            stack.push(i); //当前考察元素入栈
+            stack.push(i); // 当前考察元素入栈
         }
         return res;
     }
 
-    //结果正确，超时
+    // 结果正确，超时
     public static int largestRectangleArea(int[] heights) {
         return find(heights, 0, heights.length - 1);
     }
@@ -74,5 +74,21 @@ public class Largest_Rectangle_in_Histogram {
             }
         }
         return smallIndex;
+    }
+
+
+    // 二刷（还不会）
+    // https://leetcode.cn/problems/wiggle-subsequence/solutions/284327/tan-xin-si-lu-qing-xi-er-zheng-que-de-ti-jie-by-lg/
+    public int wiggleMaxLength_2(int[] nums) {
+        int down = 1;
+        int up = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up = down + 1;
+            } else if (nums[i] < nums[i - 1]) {
+                down = up + 1;
+            }
+        }
+        return nums.length == 0 ? 0 : Math.max(down, up);
     }
 }
