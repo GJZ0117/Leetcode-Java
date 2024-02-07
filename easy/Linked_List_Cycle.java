18f2102ac9e10269187507494e8a31237a4fc8e4
GJZ0117 <435390736@qq.com>
2024-02-07 08:24:08 +0000

    141. Linked List Cycle

--- easy/Linked_List_Cycle.java
+++ easy/Linked_List_Cycle.java
@@ -20,4 +20,22 @@ public class Linked_List_Cycle {
         }
         return false;
     }
+
+
+    // 二刷
+    public boolean hasCycle_2(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
