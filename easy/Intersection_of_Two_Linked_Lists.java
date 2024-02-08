55b4e8d9eda968eb4b43c9b61f941ccf5fa8473e
GJZ0117 <435390736@qq.com>
2024-02-08 17:24:05 +0000

    160. Intersection of Two Linked Lists

--- easy/Intersection_of_Two_Linked_Lists.java
+++ easy/Intersection_of_Two_Linked_Lists.java
@@ -6,7 +6,7 @@ package easy;
 
 public class Intersection_of_Two_Linked_Lists {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        //calculate listA and listB's length
+        // calculate listA and listB's length
         int lenA = 0;
         int lenB = 0;
         ListNode temp = headA;
@@ -20,7 +20,7 @@ public class Intersection_of_Two_Linked_Lists {
             temp = temp.next;
         }
 
-        if (lenA > lenB) { //make sure listA is shorter than listB
+        if (lenA > lenB) { // make sure listA is shorter than listB
             temp = headA;
             headA = headB;
             headB = temp;
@@ -35,11 +35,46 @@ public class Intersection_of_Two_Linked_Lists {
             diff--;
         }
 
-        while (headA!=null && headB != null && headA != headB) {
+        while (headA != null && headB != null && headA != headB) {
             headA = headA.next;
             headB = headB.next;
         }
 
         return headA;
+    }
+
+
+    // 二刷
+    public ListNode getIntersectionNode_2(ListNode headA, ListNode headB) {
+        int len1 = 0;
+        int len2 = 0;
+        ListNode cur = headA;
+        while (cur != null) {
+            cur = cur.next;
+            len1++;
+        }
+        cur = headB;
+        while (cur != null) {
+            cur = cur.next;
+            len2++;
+        }
+        if (len1 < len2) {
+            ListNode temp = headA;
+            headA = headB;
+            headB = temp;
+        }
+        int diff = Math.abs(len1 - len2);
+        while (diff > 0 && headA != null) {
+            headA = headA.next;
+            diff--;
+        }
+        while (headA != null && headB != null) {
+            if (headA == headB) {
+                return headA;
+            }
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return null;
     }
 }
