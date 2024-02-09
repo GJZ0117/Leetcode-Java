224a05d942c585336b4a65932d352f5e5b411b9f
GJZ0117 <435390736@qq.com>
2024-02-09 07:55:05 +0000

    Revert "121. Best Time to Buy and Sell Stock"
    This reverts commit d669cf07a26c9c4b960e6dbeae07ea944dbebdab.

--- easy/Best_Time_to_Buy_and_Sell_Stock.java
+++ easy/Best_Time_to_Buy_and_Sell_Stock.java
@@ -15,41 +15,15 @@ public class Best_Time_to_Buy_and_Sell_Stock {
         int min = prices[0];
         int maxDiff = prices[1] - min;
         int curDiff;
-        for (int i = 2; i < prices.length; i++) {
-            if (prices[i - 1] < min) {
-                min = prices[i - 1];
+        for (int i=2; i<prices.length; i++){
+            if (prices[i-1] < min) {
+                min = prices[i-1];
             }
             curDiff = prices[i] - min;
-            if (curDiff > maxDiff) {
+            if (curDiff > maxDiff){
                 maxDiff = curDiff;
             }
         }
         return Math.max(maxDiff, 0);
-    }
-
-
-    // 二刷
-    public ListNode mergeTwoLists_2(ListNode list1, ListNode list2) {
-        ListNode newHead = new ListNode();
-        ListNode cur = newHead;
-        ListNode l1 = list1;
-        ListNode l2 = list2;
-        while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                cur.next = l1;
-                l1 = l1.next;
-            } else {
-                cur.next = l2;
-                l2 = l2.next;
-            }
-            cur = cur.next;
-        }
-
-        if (l1 != null) {
-            cur.next = l1;
-        } else if (l2 != null) {
-            cur.next = l2;
-        }
-        return newHead.next;
     }
 }
