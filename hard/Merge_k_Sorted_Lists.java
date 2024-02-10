10f3debe9b15011128b05d84b8a7870ae55bb20a
GJZ0117 <435390736@qq.com>
2024-02-10 09:40:51 +0000

    23. Merge k Sorted Lists

--- hard/Merge_k_Sorted_Lists.java
+++ hard/Merge_k_Sorted_Lists.java
@@ -2,13 +2,14 @@ package hard;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * 23. Merge k Sorted Lists
  */
 
 
-//class ListNode {
+// class ListNode {
 //    int val;
 //    ListNode next;
 //
@@ -28,31 +29,31 @@ import java.util.List;
 public class Merge_k_Sorted_Lists {
 
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists.length == 0) { //空数组返回null
+        if (lists.length == 0) { // 空数组返回null
             return null;
         }
-        ListNode head = new ListNode(); //头节点保证对所有节点操作一致
-        ListNode cur = head; //遍历并链接所有链表
-        ListNode tempMin; //记录每次使用findMin方法找到的集合中最小的节点
+        ListNode head = new ListNode(); // 头节点保证对所有节点操作一致
+        ListNode cur = head; // 遍历并链接所有链表
+        ListNode tempMin; // 记录每次使用findMin方法找到的集合中最小的节点
         List<ListNode> list = new ArrayList<>();
-        for (ListNode node : lists) { //把每个链表中的最小的节点（第一个节点）加入集合
-            if (node != null) { //为空的链表（如[[1,2,3], [], []]）直接跳过
+        for (ListNode node : lists) { // 把每个链表中的最小的节点（第一个节点）加入集合
+            if (node != null) { // 为空的链表（如[[1,2,3], [], []]）直接跳过
                 list.add(node);
             }
         }
-        while (!list.isEmpty()) { //集合为空说明所有链表的所有节点都遍历过
-            tempMin = findMin(list); //找到当前集合中最小的节点
-            cur.next = tempMin; //把当前集合中最小节点加到主链表上
-            cur = cur.next; //遍历指针移到主链表最后一个节点
-            list.remove(tempMin); //从集合中删除刚找出的最小节点
-            if (tempMin.next != null) { //若最小节点的下一个节点不为空，则将其加入到集合中
+        while (!list.isEmpty()) { // 集合为空说明所有链表的所有节点都遍历过
+            tempMin = findMin(list); // 找到当前集合中最小的节点
+            cur.next = tempMin; // 把当前集合中最小节点加到主链表上
+            cur = cur.next; // 遍历指针移到主链表最后一个节点
+            list.remove(tempMin); // 从集合中删除刚找出的最小节点
+            if (tempMin.next != null) { // 若最小节点的下一个节点不为空，则将其加入到集合中
                 list.add(tempMin.next);
             }
         }
         return head.next;
     }
 
-    public static ListNode findMin(List<ListNode> list) { //从保存每个链表最小节点的集合中找到最小的节点
+    public static ListNode findMin(List<ListNode> list) { // 从保存每个链表最小节点的集合中找到最小的节点
         ListNode minNode = list.get(0);
         int min = list.get(0).val;
         for (int i = 1; i < list.size(); i++) {
@@ -62,5 +63,31 @@ public class Merge_k_Sorted_Lists {
             }
         }
         return minNode;
+    }
+
+
+    // 二刷
+    public ListNode mergeKLists_2(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+        PriorityQueue<ListNode> heap = new PriorityQueue<>((a, b) -> a.val - b.val);
+        ListNode newHead = new ListNode();
+        ListNode cur = newHead;
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] != null) {
+                heap.add(lists[i]);
+            }
+        }
+        while (!heap.isEmpty()) {
+            ListNode min = heap.poll();
+            if (min.next != null) {
+                heap.add(min.next);
+            }
+            min.next = null;
+            cur.next = min;
+            cur = cur.next;
+        }
+        return newHead.next;
     }
 }
