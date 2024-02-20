dd1af182c16faa1ba721083867c5d7d60cc00298
Jingzhe Gao <435390736@qq.com>
2024-02-20 08:12:26 +0000

    344. Reverse String

--- easy/Reverse_String.java
+++ easy/Reverse_String.java
@@ -17,4 +17,18 @@ public class Reverse_String {
             j--;
         }
     }
+
+
+    // 二刷
+    public void reverseString_2(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char c = s[left];
+            s[left] = s[right];
+            s[right] = c;
+            left++;
+            right--;
+        }
+    }
 }
