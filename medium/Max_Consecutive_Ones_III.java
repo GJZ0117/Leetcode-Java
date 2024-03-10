1e282d20057a6dbac0f8671050fd39dfe72b9eb8
Jingzhe Gao <435390736@qq.com>
2024-03-10 08:10:16 +0000

    1004. Max Consecutive Ones III

--- medium/Max_Consecutive_Ones_III.java
+++ medium/Max_Consecutive_Ones_III.java
@@ -25,4 +25,27 @@ public class Max_Consecutive_Ones_III {
         }
         return maxLength;
     }
+
+
+    // 二刷
+    public int longestOnes_2(int[] nums, int k) {
+        int left = 0;
+        int right = 0;
+        int maxLen = 0;
+        int remain = k;
+        while (right < nums.length) {
+            if (nums[right] == 0) {
+                remain--;
+            }
+            while (remain < 0) {
+                if (nums[left] == 0) {
+                    remain++;
+                }
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+            right++;
+        }
+        return maxLen;
+    }
 }
