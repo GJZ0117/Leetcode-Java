f4a91604efb442cce68dec43cce5d9fd97c84cdf
Jingzhe Gao <435390736@qq.com>
2024-03-17 20:30:44 +0000

    713. Subarray Product Less Than K

--- medium/Subarray_Product_Less_Than_K.java
+++ medium/Subarray_Product_Less_Than_K.java
@@ -25,4 +25,29 @@ public class Subarray_Product_Less_Than_K {
         }
         return ans;
     }
+
+
+    // 二刷
+    public int numSubarrayProductLessThanK_2(int[] nums, int k) {
+        if (k == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = 0;
+        int multi = 1;
+        int ans = 0;
+        while (right < nums.length) {
+            multi *= nums[right];
+            if (multi < k) {
+                ans += right - left + 1;
+            } else {
+                while (left <= right && multi >= k) {
+                    multi /= nums[left++];
+                }
+                ans += right - left + 1;
+            }
+            right++;
+        }
+        return ans;
+    }
 }
