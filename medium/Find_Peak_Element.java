84ef12d15bdf9893debfc86e05c9b7f1542ef13b
Jingzhe Gao <435390736@qq.com>
2024-03-18 16:52:45 +0000

    162. Find Peak Element

--- medium/Find_Peak_Element.java
+++ medium/Find_Peak_Element.java
@@ -64,4 +64,20 @@ public class Find_Peak_Element {
         }
         return -1;
     }
+
+
+    // 三刷
+    public int findPeakElement_3(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
 }
