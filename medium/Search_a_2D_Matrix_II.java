02e73f1d27409c0b17af8f5d6ccee332bf12c381
Jingzhe Gao <435390736@qq.com>
2024-03-19 23:08:42 +0000

    240. Search a 2D Matrix II

--- medium/Search_a_2D_Matrix_II.java
+++ medium/Search_a_2D_Matrix_II.java
@@ -58,4 +58,28 @@ public class Search_a_2D_Matrix_II {
 
         return false;
     }
+
+
+    // 三刷
+    public boolean searchMatrix_3(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] <= target && matrix[i][n - 1] >= target) {
+                int left = 0;
+                int right = n - 1;
+                while (left <= right) {
+                    int mid = left + (right - left) / 2;
+                    if (matrix[i][mid] == target) {
+                        return true;
+                    } else if (matrix[i][mid] > target) {
+                        right = mid - 1;
+                    } else if (matrix[i][mid] < target) {
+                        left = mid + 1;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
