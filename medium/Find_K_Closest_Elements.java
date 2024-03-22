80216d2d126be3c42b58dc292c02aaa98f0f4a38
Jingzhe Gao <435390736@qq.com>
2024-03-22 09:17:47 +0000

    658. Find K Closest Elements

--- medium/Find_K_Closest_Elements.java
+++ medium/Find_K_Closest_Elements.java
@@ -60,4 +60,33 @@ public class Find_K_Closest_Elements {
         Collections.sort(ans);
         return ans;
     }
+
+
+    // 三刷
+    public List<Integer> findClosestElements_3(int[] arr, int k, int x) {
+        TreeMap<Integer, List<Integer>> map = new TreeMap<>();
+        for (int n : arr) {
+            int key = Math.abs(n - x);
+            if (map.containsKey(key)) {
+                map.get(key).add(n);
+            } else {
+                List<Integer> list = new ArrayList<>();
+                list.add(n);
+                map.put(key, list);
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        while (k > 0) {
+            Map.Entry<Integer, List<Integer>> entry = map.firstEntry();
+            Integer key = entry.getKey();
+            List<Integer> list = entry.getValue();
+            map.remove(key);
+            for (int i = 0; i < list.size() && k > 0; i++) {
+                ans.add(list.get(i));
+                k--;
+            }
+        }
+        Collections.sort(ans);
+        return ans;
+    }
 }
