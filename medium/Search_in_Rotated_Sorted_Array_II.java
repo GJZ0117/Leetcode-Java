b716ce528981923d227f743400a60e64e23bbb79
Jingzhe Gao <435390736@qq.com>
2024-03-24 21:38:46 +0000

    81. Search in Rotated Sorted Array II

--- medium/Search_in_Rotated_Sorted_Array_II.java
+++ medium/Search_in_Rotated_Sorted_Array_II.java
@@ -13,20 +13,20 @@ public class Search_in_Rotated_Sorted_Array_II {
             mid = (low + high) / 2;
             if (nums[mid] == target) {
                 return true;
-            } else if (nums[mid] < nums[high] || nums[mid] < nums[low]) { //right side is sorted or left side is unsorted
+            } else if (nums[mid] < nums[high] || nums[mid] < nums[low]) { // right side is sorted or left side is unsorted
                 if (target > nums[mid] && target <= nums[high]) {
                     low = mid + 1;
                 } else {
                     high = mid - 1;
                 }
-            } else if (nums[mid] > nums[low] || nums[mid] > nums[high]) { //left side is sorted or right side is unsorted
+            } else if (nums[mid] > nums[low] || nums[mid] > nums[high]) { // left side is sorted or right side is unsorted
                 if (target < nums[mid] && target >= nums[low]) {
                     high = mid - 1;
                 } else {
                     low = mid + 1;
                 }
-            } else { //nums[start] == nums[mid] == nums[end]
-                high--; //or low++ is ok
+            } else { // nums[start] == nums[mid] == nums[end]
+                high--; // or low++ is ok
             }
         }
         return false;
@@ -66,6 +66,37 @@ public class Search_in_Rotated_Sorted_Array_II {
             }
         }
 
+        return false;
+    }
+
+
+    // 三刷
+    public boolean search_3(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[left] == nums[mid]) {
+                left++;
+                continue;
+            }
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
         return false;
     }
 }
