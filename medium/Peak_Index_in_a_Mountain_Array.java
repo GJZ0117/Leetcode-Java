496ce85c97815b99a4cbd99ce27606624709322f
Jingzhe Gao <435390736@qq.com>
2024-03-25 22:07:26 +0000

    852. Peak Index in a Mountain Array

--- medium/Peak_Index_in_a_Mountain_Array.java
+++ medium/Peak_Index_in_a_Mountain_Array.java
@@ -13,4 +13,21 @@ public class Peak_Index_in_a_Mountain_Array {
         }
         return -1;
     }
+
+    // 二刷
+    public int peakIndexInMountainArray_2(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) {
+                return mid;
+            } else if (arr[mid] > arr[mid + 1]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
