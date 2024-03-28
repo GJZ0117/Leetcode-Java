564b6e754452c74c694ea70e8e5483ecf9776d1f
Jingzhe Gao <435390736@qq.com>
2024-03-28 22:14:32 +0000

    870. Advantage Shuffle

--- medium/Advantage_Shuffle.java
+++ medium/Advantage_Shuffle.java
@@ -1,6 +1,7 @@
 package medium;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -25,6 +26,39 @@ public class Advantage_Shuffle {
             } else {
                 ans[index] = nums1[i];
                 i++;
+            }
+        }
+        return ans;
+    }
+
+
+    // 二刷
+    public int[] advantageCount_2(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] pair1, int[] pair2) {
+                return pair2[1] - pair1[1];
+            }
+        });
+        for (int i = 0; i < n; i++) {
+            maxHeap.add(new int[]{i, nums2[i]});
+        }
+        Arrays.sort(nums1);
+
+        int[] ans = new int[n];
+        int left = 0;
+        int right = n - 1;
+        while (!maxHeap.isEmpty()) {
+            int[] pair = maxHeap.poll();
+            int idx = pair[0];
+            int maxValue = pair[1];
+            if (maxValue < nums1[right]) {
+                ans[idx] = nums1[right];
+                right--;
+            } else {
+                ans[idx] = nums1[left];
+                left++;
             }
         }
         return ans;
