3d99e3790e76a1312ac70f95dde2da1776e59309
Jingzhe Gao <435390736@qq.com>
2024-04-02 19:30:37 +0100

    921. Minimum Add to Make Parentheses Valid

--- medium/Minimum_Add_to_Make_Parentheses_Valid.java
+++ medium/Minimum_Add_to_Make_Parentheses_Valid.java
@@ -19,4 +19,23 @@ public class Minimum_Add_to_Make_Parentheses_Valid {
         }
         return deque.size();
     }
+
+
+    // 二刷
+    public int minAddToMakeValid_2(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.addLast('(');
+            } else {
+                if (stack.isEmpty()) {
+                    count++;
+                } else {
+                    stack.pollLast();
+                }
+            }
+        }
+        return count + stack.size();
+    }
 }
