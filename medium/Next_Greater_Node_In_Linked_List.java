39ef47cafd1718e1ec6d3551ffdd8e53723dd497
Jingzhe Gao <435390736@qq.com>
2024-04-08 22:30:54 +0100

    1019. Next Greater Node In Linked List

--- medium/Next_Greater_Node_In_Linked_List.java
+++ medium/Next_Greater_Node_In_Linked_List.java
@@ -1,7 +1,6 @@
 package medium;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 /**
  * 1019. Next Greater Node In Linked List
@@ -29,5 +28,34 @@ public class Next_Greater_Node_In_Linked_List {
             ans[i] = stack.peek();
         }
         stack.push(node.val);
+    }
+
+
+    // 二刷
+    public int[] nextLargerNodes_2(ListNode head) {
+        int len = 0;
+        ListNode node = head;
+        Map<Integer, Integer> map = new HashMap<>();
+        int index = 0;
+        while (node != null) {
+            map.put(index++, node.val);
+            len++;
+            node = node.next;
+        }
+
+        int[] ans = new int[len];
+        Deque<Integer> stack = new LinkedList<>();
+
+        node = head;
+        index = 0;
+        while (node != null) {
+            while (!stack.isEmpty() && node.val > map.get(stack.peekLast())) {
+                int top = stack.pollLast();
+                ans[top] = node.val;
+            }
+            stack.addLast(index++);
+            node = node.next;
+        }
+        return ans;
     }
 }
