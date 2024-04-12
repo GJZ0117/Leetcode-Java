7af9ddf508b07161722d667943687dbf5a3530a4
Jingzhe Gao <435390736@qq.com>
2024-04-12 19:55:33 +0100

    918. Maximum Sum Circular Subarray

--- medium/Maximum_Sum_Circular_Subarray.java
+++ medium/Maximum_Sum_Circular_Subarray.java
@@ -21,4 +21,23 @@ public class Maximum_Sum_Circular_Subarray {
         }
         return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
     }
+
+
+    // 二刷
+    public int maxSubarraySumCircular_2(int[] nums) {
+        int total = 0;
+        int maxSum = nums[0];
+        int curMax = 0;
+        int minSum = nums[0];
+        int curMin = 0;
+
+        for (int num : nums) {
+            curMax = Math.max(curMax + num, num);
+            maxSum = Math.max(maxSum, curMax);
+            curMin = Math.min(curMin + num, num);
+            minSum = Math.min(minSum, curMin);
+            total += num;
+        }
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+    }
 }
