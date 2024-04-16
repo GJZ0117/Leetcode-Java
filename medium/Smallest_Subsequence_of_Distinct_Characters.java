5fc24da779dbadc528535079c09f83d1adb4a1f0
Jingzhe Gao <435390736@qq.com>
2024-04-16 22:33:21 +0100

    1081. Smallest Subsequence of Distinct Characters

---
+++ medium/Smallest_Subsequence_of_Distinct_Characters.java
@@ -0,0 +1,34 @@
+package medium;
+
+/**
+ * 1081. Smallest Subsequence of Distinct Characters
+ */
+
+public class Smallest_Subsequence_of_Distinct_Characters {
+    public String smallestSubsequence(String s) {
+        boolean[] visited = new boolean[26];
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!visited[ch - 'a']) {
+                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
+                    if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                        visited[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else {
+                        break;
+                    }
+                }
+                visited[ch - 'a'] = true;
+                sb.append(ch);
+            }
+            num[ch - 'a'] -= 1;
+        }
+        return sb.toString();
+    }
+}
