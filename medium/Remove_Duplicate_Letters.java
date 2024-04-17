2cfe5a3bcaa0159abae59112c11247e84c1a76a0
Jingzhe Gao <435390736@qq.com>
2024-04-17 23:03:20 +0100

    Remove Duplicate Letters

--- medium/Remove_Duplicate_Letters.java
+++ medium/Remove_Duplicate_Letters.java
@@ -72,4 +72,33 @@ public class Remove_Duplicate_Letters {
         }
         return sb.toString();
     }
+
+
+    // 三刷
+    public String removeDuplicateLetters_3(String s) {
+        boolean[] visited = new boolean[26];
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!visited[ch - 'a']) {
+                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
+                    if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                        visited[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else {
+                        break;
+                    }
+                }
+                visited[ch - 'a'] = true;
+                sb.append(ch);
+            }
+            num[ch - 'a'] -= 1;
+        }
+        return sb.toString();
+    }
 }
