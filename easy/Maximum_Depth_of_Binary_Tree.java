4861857188aa51be33b0eaae382a4d47436303de
Jingzhe Gao <435390736@qq.com>
2024-04-19 18:23:41 +0100

    104. Maximum Depth of Binary Tree

--- easy/Maximum_Depth_of_Binary_Tree.java
+++ easy/Maximum_Depth_of_Binary_Tree.java
@@ -13,4 +13,15 @@ public class Maximum_Depth_of_Binary_Tree {
         int right = maxDepth(root.right) + 1;
         return Math.max(left, right);
     }
+
+
+    // 二刷
+    public int maxDepth_2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth_2(root.left) + 1;
+        int right = maxDepth_2(root.right) + 1;
+        return Math.max(left, right);
+    }
 }
