dd845cc9f9f7dc92b82c6f84b021a509657d6623
Jingzhe Gao <435390736@qq.com>
2024-04-20 18:24:18 +0100

    543. Diameter of Binary Tree

--- easy/Diameter_of_Binary_Tree.java
+++ easy/Diameter_of_Binary_Tree.java
@@ -20,4 +20,23 @@ public class Diameter_of_Binary_Tree {
         diameter[0] = Math.max(diameter[0], left + right);
         return Math.max(left, right);
     }
+
+
+    // 二刷
+    public int diameterOfBinaryTree_2(TreeNode root) {
+        post(root);
+        return ans;
+    }
+
+    int ans = 0;
+
+    public int post(TreeNode node) {
+        if (node == null) {
+            return -1;
+        }
+        int left = post(node.left) + 1;
+        int right = post(node.right) + 1;
+        ans = Math.max(ans, left + right);
+        return Math.max(left, right);
+    }
 }
