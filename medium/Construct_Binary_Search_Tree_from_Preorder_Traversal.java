447dbfaa42bc4fe48181cb2af0718e03bbbdea3c
Jingzhe Gao <435390736@qq.com>
2024-04-21 22:30:04 +0100

    1008. Construct Binary Search Tree from Preorder Traversal

--- medium/Construct_Binary_Search_Tree_from_Preorder_Traversal.java
+++ medium/Construct_Binary_Search_Tree_from_Preorder_Traversal.java
@@ -24,4 +24,37 @@ public class Construct_Binary_Search_Tree_from_Preorder_Traversal {
         }
         return node;
     }
+
+
+    // 二刷
+    public TreeNode bstFromPreorder_2(int[] preorder) {
+        TreeNode root = new TreeNode();
+        root.val = preorder[0];
+        for (int i = 1; i < preorder.length; i++) {
+            buildTree(root, preorder[i]);
+        }
+        return root;
+    }
+
+    public void buildTree(TreeNode root, int val) {
+        TreeNode node = new TreeNode(val);
+        TreeNode p = root;
+        while (true) {
+            if (p.val > val) {
+                if (p.left == null) {
+                    p.left = node;
+                    break;
+                } else {
+                    p = p.left;
+                }
+            } else {
+                if (p.right == null) {
+                    p.right = node;
+                    break;
+                } else {
+                    p = p.right;
+                }
+            }
+        }
+    }
 }
