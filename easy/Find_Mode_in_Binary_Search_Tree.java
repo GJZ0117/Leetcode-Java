d9be7bb0821f34911ead12d255372c4d5765d490
Jingzhe Gao <435390736@qq.com>
2024-05-18 10:55:57 +0100

    501. Find Mode in Binary Search Tree

--- easy/Find_Mode_in_Binary_Search_Tree.java
+++ easy/Find_Mode_in_Binary_Search_Tree.java
@@ -1,7 +1,6 @@
 package easy;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 501. Find Mode in Binary Search Tree
@@ -45,5 +44,35 @@ public class Find_Mode_in_Binary_Search_Tree {
             }
             inOrder(cur.right);
         }
+    }
+
+
+    // 二刷
+    public int[] findMode_2(TreeNode root) {
+        dfs(root, new HashMap<>());
+        int[] arr = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            arr[i] = ans.get(i);
+        }
+        return arr;
+    }
+
+    int maxNum = 0;
+    List<Integer> ans = new ArrayList<>();
+
+    public void dfs(TreeNode node, Map<Integer, Integer> map) {
+        if (node == null) {
+            return;
+        }
+        map.put(node.val, map.getOrDefault(node.val, 0) + 1);
+        if (map.get(node.val) > maxNum) {
+            maxNum = map.get(node.val);
+            ans.clear();
+            ans.add(node.val);
+        } else if (map.get(node.val) == maxNum) {
+            ans.add(node.val);
+        }
+        dfs(node.left, map);
+        dfs(node.right, map);
     }
 }
