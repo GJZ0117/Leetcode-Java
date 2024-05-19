6e837a18e32ec603c9c4a7b18f5ca487a2096d6c
Jingzhe Gao <435390736@qq.com>
2024-05-19 10:06:29 +0100

    508. Most Frequent Subtree Sum

--- medium/Most_Frequent_Subtree_Sum.java
+++ medium/Most_Frequent_Subtree_Sum.java
@@ -1,6 +1,8 @@
 package medium;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -40,6 +42,41 @@ public class Most_Frequent_Subtree_Sum {
         int leftChildTree = postOrder(node.left, map);
         int rightChildTree = postOrder(node.right, map);
         int cur = node.val + leftChildTree + rightChildTree;
+        map.put(cur, map.getOrDefault(cur, 0) + 1);
+        return cur;
+    }
+
+
+    // 二刷
+    public int[] findFrequentTreeSum_2(TreeNode root) {
+        Map<Integer, Integer> map = new HashMap<>();
+        postOrderTraversal(root, map);
+        int maxFrequency = 0;
+        for (int frequency : map.values()) {
+            if (frequency > maxFrequency) {
+                maxFrequency = frequency;
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int key : map.keySet()) {
+            if (map.get(key) == maxFrequency) {
+                list.add(key);
+            }
+        }
+        int[] arr = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+
+    public int postOrderTraversal(TreeNode node, Map<Integer, Integer> map) {
+        if (node == null) {
+            return 0;
+        }
+        int left = postOrderTraversal(node.left, map);
+        int right = postOrderTraversal(node.right, map);
+        int cur = left + right + node.val;
         map.put(cur, map.getOrDefault(cur, 0) + 1);
         return cur;
     }
