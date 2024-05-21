22b2c578565138b52b8af11a138371bb093f32ad
Jingzhe Gao <435390736@qq.com>
2024-05-21 21:26:20 +0100

    515. Find Largest Value in Each Tree Row

--- medium/Find_Largest_Value_in_Each_Tree_Row.java
+++ medium/Find_Largest_Value_in_Each_Tree_Row.java
@@ -1,8 +1,6 @@
 package medium;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 515. Find Largest Value in Each Tree Row
@@ -32,6 +30,37 @@ public class Find_Largest_Value_in_Each_Tree_Row {
                 len--;
             }
             list.add(maxValue);
+        }
+        return list;
+    }
+
+
+    // 二刷
+    public List<Integer> largestValues_2(TreeNode root) {
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        queue.addLast(root);
+        TreeNode p;
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            int maxVal = Integer.MIN_VALUE;
+            while (len > 0) {
+                p = queue.pollFirst();
+                if (p.val > maxVal) {
+                    maxVal = p.val;
+                }
+                if (p.left != null) {
+                    queue.addLast(p.left);
+                }
+                if (p.right != null) {
+                    queue.addLast(p.right);
+                }
+                len--;
+            }
+            list.add(maxVal);
         }
         return list;
     }
