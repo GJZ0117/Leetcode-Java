47190be2a8427772600e17c00be992f7f032a683
Jingzhe Gao <435390736@qq.com>
2024-06-04 22:16:37 +0100

    606. Construct String from Binary Tree

--- medium/Construct_String_from_Binary_Tree.java
+++ medium/Construct_String_from_Binary_Tree.java
@@ -1,4 +1,4 @@
-package easy;
+package medium;
 
 /**
  * 606. Construct String from Binary Tree
@@ -28,6 +28,36 @@ public class Construct_String_from_Binary_Tree {
                 preOrder(cur.right, sb);
                 sb.append(")");
             }
+        }
+    }
+
+
+    // 二刷
+    public String tree2str_2(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        preOrderTraversal(root, sb);
+        return sb.toString();
+    }
+
+    public void preOrderTraversal(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            return;
+        }
+
+        sb.append(node.val);
+
+        if (node.left == null && node.right == null) {
+            return;
+        }
+
+        sb.append("(");
+        preOrderTraversal(node.left, sb);
+        sb.append(")");
+
+        if (node.right != null) {
+            sb.append("(");
+            preOrderTraversal(node.right, sb);
+            sb.append(")");
         }
     }
 }
