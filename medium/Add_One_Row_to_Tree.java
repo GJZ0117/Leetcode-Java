4616c17e990ab1fff47a1080d8ec97d8cea46abe
Jingzhe Gao <435390736@qq.com>
2024-06-14 23:50:07 +0800

    623. Add One Row to Tree

--- medium/Add_One_Row_to_Tree.java
+++ medium/Add_One_Row_to_Tree.java
@@ -1,5 +1,8 @@
 package medium;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * 623. Add One Row to Tree
  */
@@ -29,6 +32,41 @@ public class Add_One_Row_to_Tree {
                 addOneRow(root.left, val, depth - 1);
                 addOneRow(root.right, val, depth - 1);
             }
+        }
+        return root;
+    }
+
+
+    // 二刷
+    public TreeNode addOneRow_2(TreeNode root, int val, int depth) {
+        if (depth == 1) {
+            return new TreeNode(val, root, null);
+        }
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.addLast(root);
+        int curDepth = 1;
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            while (len > 0) {
+                TreeNode node = queue.removeFirst();
+                if (curDepth == depth - 1) {
+                    TreeNode newNode1 = new TreeNode(val);
+                    TreeNode newNode2 = new TreeNode(val);
+                    newNode1.left = node.left;
+                    node.left = newNode1;
+                    newNode2.right = node.right;
+                    node.right = newNode2;
+                } else {
+                    if (node.left != null) {
+                        queue.addLast(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.addLast(node.right);
+                    }
+                }
+                len--;
+            }
+            curDepth++;
         }
         return root;
     }
