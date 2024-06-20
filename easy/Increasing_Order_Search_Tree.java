56e79fdd0f3b99faeac61e86516baf9186708087
Jingzhe Gao <435390736@qq.com>
2024-06-21 07:20:51 +0800

    897. Increasing Order Search Tree

--- easy/Increasing_Order_Search_Tree.java
+++ easy/Increasing_Order_Search_Tree.java
@@ -20,12 +20,38 @@ public class Increasing_Order_Search_Tree {
             if (pre == null) {
                 resRoot = node;
                 pre = node;
-            }else {
+            } else {
                 pre.right = node;
                 node.left = null;
                 pre = node;
             }
             inOrder(node.right);
         }
+    }
+
+
+    // 二刷
+    public TreeNode increasingBST_2(TreeNode root) {
+        inOrderTraversal(root);
+        return newRoot;
+    }
+
+    TreeNode newRoot = null;
+    TreeNode preNode = null;
+
+    public void inOrderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inOrderTraversal(node.left);
+        if (newRoot == null) {
+            newRoot = node;
+            preNode = node;
+        } else {
+            node.left = null;
+            preNode.right = node;
+            preNode = node;
+        }
+        inOrderTraversal(node.right);
     }
 }
