ebb55864bad8bc54271f22cedfc6b140239e1ca5
Jingzhe Gao <435390736@qq.com>
2024-06-20 08:04:36 +0800

    894. All Possible Full Binary Trees

--- medium/All_Possible_Full_Binary_Trees.java
+++ medium/All_Possible_Full_Binary_Trees.java
@@ -34,4 +34,34 @@ public class All_Possible_Full_Binary_Trees {
         }
         return list;
     }
+
+
+    // 二刷（还不会）
+    // https://leetcode.cn/problems/all-possible-full-binary-trees/solutions/2719920/you-yi-dao-mo-ban-ti-dan-geng-ling-huo-x-j03j/?show=1
+    public List<TreeNode> allPossibleFBT_2(int n) {
+        return build(n);
+    }
+
+    public List<TreeNode> build(int n) {
+        List<TreeNode> res = new ArrayList<>();
+        if (n == 1) {
+            res.add(new TreeNode(0));
+            return res;
+        }
+        if (n % 2 == 0) {
+            return res;
+        }
+
+        for (int i = 1; i < n; i += 2) {
+            List<TreeNode> leftTree = build(i);
+            List<TreeNode> rightTree = build(n - i - 1);
+            for (TreeNode left : leftTree) {
+                for (TreeNode right : rightTree) {
+                    TreeNode node = new TreeNode(0, left, right);
+                    res.add(node);
+                }
+            }
+        }
+        return res;
+    }
 }
