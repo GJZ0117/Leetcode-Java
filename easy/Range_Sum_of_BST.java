e5ecceb83ebfa8e1489954ba5fc34aa1f64a004b
Jingzhe Gao <435390736@qq.com>
2024-06-22 19:53:56 +0800

    938. Range Sum of BST

--- easy/Range_Sum_of_BST.java
+++ easy/Range_Sum_of_BST.java
@@ -17,4 +17,20 @@ public class Range_Sum_of_BST {
         }
         return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
     }
+
+
+    // 二刷
+    public int rangeSumBST_2(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.val < low) {
+            return rangeSumBST_2(root.right, low, high);
+        }
+        if (root.val > high) {
+            return rangeSumBST_2(root.left, low, high);
+        }
+        return root.val + rangeSumBST_2(root.left, low, high) + rangeSumBST_2(root.right, low, high);
+
+    }
 }
