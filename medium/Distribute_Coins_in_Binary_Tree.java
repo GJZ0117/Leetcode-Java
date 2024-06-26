852bc8b34a639e3601e81f6e85b35166aaeb5bfb
Jingzhe Gao <435390736@qq.com>
2024-06-27 07:55:08 +0800

    979. Distribute Coins in Binary Tree

--- medium/Distribute_Coins_in_Binary_Tree.java
+++ medium/Distribute_Coins_in_Binary_Tree.java
@@ -23,4 +23,27 @@ public class Distribute_Coins_in_Binary_Tree {
             return 0;
         }
     }
+
+
+    // 二刷
+    // https://leetcode.cn/problems/distribute-coins-in-binary-tree/solutions/265089/hou-xu-bian-li-cong-dang-qian-jie-dian-na-chu-node/?show=1
+    public int distributeCoins_2(TreeNode root) {
+        if (dfs(root) == 0) {
+            return ans;
+        }
+        return -1;
+    }
+
+    int ans = 0;
+
+    public int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = dfs(node.left);
+        ans += Math.abs(left);
+        int right = dfs(node.right);
+        ans += Math.abs(right);
+        return left + right + node.val - 1;
+    }
 }
