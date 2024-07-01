2bbf4ff213bcf69c69338540f3e100f19e9efc22
Jingzhe Gao <435390736@qq.com>
2024-07-01 08:14:01 +0800

    998. Maximum Binary Tree II

--- medium/Maximum_Binary_Tree_II.java
+++ medium/Maximum_Binary_Tree_II.java
@@ -16,4 +16,18 @@ public class Maximum_Binary_Tree_II {
         node.right = dfs(node.right, val);
         return node;
     }
+
+
+    // 二刷
+    public TreeNode insertIntoMaxTree_2(TreeNode root, int val) {
+        return insert(root, val);
+    }
+
+    public TreeNode insert(TreeNode node, int val) {
+        if (node == null || node.val < val) {
+            return new TreeNode(val, node, null);
+        }
+        node.right = insert(node.right, val);
+        return node;
+    }
 }
