b6a08ac9fe8b7ea5b58ba987673e501a25b30346
Jingzhe Gao <435390736@qq.com>
2024-07-08 08:10:09 +0800

    912. Sort an Array

--- medium/Sort_an_Array.java
+++ medium/Sort_an_Array.java
@@ -35,4 +35,37 @@ public class Sort_an_Array {
         }
         nums[parent] = temp;
     }
+
+
+    // 二刷
+    public int[] sortArray_2(int[] nums) {
+        for (int i = (nums.length - 1) / 2; i >= 0; i--) {
+            adjustHeap_2(nums, i, nums.length);
+        }
+        for (int i = nums.length - 1; i > 0; i--) {
+            int temp = nums[i];
+            nums[i] = nums[0];
+            nums[0] = temp;
+            adjustHeap_2(nums, 0, i);
+        }
+        return nums;
+    }
+
+    public void adjustHeap_2(int[] nums, int parent, int length) {
+        int temp = nums[parent];
+        int lChild = parent * 2 + 1;
+        while (lChild < length) {
+            int rChild = lChild + 1;
+            if (rChild < length && nums[lChild] < nums[rChild]) {
+                lChild++;
+            }
+            if (temp >= nums[lChild]) {
+                break;
+            }
+            nums[parent] = nums[lChild];
+            parent = lChild;
+            lChild = 2 * lChild + 1;
+        }
+        nums[parent] = temp;
+    }
 }
