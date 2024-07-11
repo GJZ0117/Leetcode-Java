f91f16c74ea3a592460f62332756183c151db8d9
Jingzhe Gao <435390736@qq.com>
2024-07-11 08:41:03 +0800

    450. Delete Node in a BST

--- medium/Delete_Node_in_a_BST.java
+++ medium/Delete_Node_in_a_BST.java
@@ -29,4 +29,33 @@ public class Delete_Node_in_a_BST {
         }
         return root;
     }
+
+
+    // 二刷
+    public TreeNode deleteNode_2(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == key) {
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+            TreeNode rightTreeMinNode = root.right;
+            while (rightTreeMinNode.left != null) {
+                rightTreeMinNode = rightTreeMinNode.left;
+            }
+            root.right = deleteNode_2(root.right, rightTreeMinNode.val);
+            rightTreeMinNode.left = root.left;
+            rightTreeMinNode.right = root.right;
+            return rightTreeMinNode;
+        } else if (root.val > key) {
+            root.left = deleteNode_2(root.left, key);
+        } else {
+            root.right = deleteNode_2(root.right, key);
+        }
+        return root;
+    }
 }
