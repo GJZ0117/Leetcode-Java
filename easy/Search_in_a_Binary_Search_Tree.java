2e6f5e753de0a59be82fa910a4a434ee74e9d052
Jingzhe Gao <435390736@qq.com>
2024-07-12 22:11:26 +0800

    700. Search in a Binary Search Tree

--- easy/Search_in_a_Binary_Search_Tree.java
+++ easy/Search_in_a_Binary_Search_Tree.java
@@ -18,4 +18,19 @@ public class Search_in_a_Binary_Search_Tree {
             }
         }
     }
+
+
+    // 二刷
+    public TreeNode searchBST_2(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST_2(root.left, val);
+        } else {
+            return searchBST_2(root.right, val);
+        }
+    }
 }
