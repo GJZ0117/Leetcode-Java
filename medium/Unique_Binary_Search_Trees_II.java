6cc2b30d341e11a3210a90acd65f692fc5d3dace
Jingzhe Gao <435390736@qq.com>
2024-07-15 07:59:22 +0800

    95. Unique Binary Search Trees II

--- medium/Unique_Binary_Search_Trees_II.java
+++ medium/Unique_Binary_Search_Trees_II.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 
 public class Unique_Binary_Search_Trees_II {
-    //dp
+    // dp
     public List<TreeNode> generateTrees(int n) {
         List<TreeNode>[] result = new ArrayList[n + 1];
         result[0] = new ArrayList<>();
@@ -69,6 +69,37 @@ public class Unique_Binary_Search_Trees_II {
             }
         }
 
+        return list;
+    }
+
+
+    // 三刷
+    public List<TreeNode> generateTrees_3(int n) {
+        if (n < 1) {
+            return new ArrayList<>();
+        }
+        return build(1, n);
+    }
+
+    public List<TreeNode> build(int start, int end) {
+        List<TreeNode> list = new ArrayList<>();
+        if (start > end) {
+            list.add(null);
+            return list;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> left = build(start, i - 1);
+            List<TreeNode> right = build(i + 1, end);
+
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    list.add(root);
+                }
+            }
+        }
         return list;
     }
 }
