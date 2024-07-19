dafb0a2bcd2244a33ce61b9d0edc3290c0f2cc4e
Jingzhe Gao <435390736@qq.com>
2024-07-19 08:15:14 +0800

    133. Clone Graph

--- medium/Clone_Graph.java
+++ medium/Clone_Graph.java
@@ -64,35 +64,74 @@ public class Clone_Graph {
 
     // 二刷
     public GraphNode cloneGraph_2(GraphNode node) {
-        if(node == null) {
+        if (node == null) {
             return node;
         }
         Map<Integer, GraphNode> map = new HashMap<>();
         Deque<GraphNode> deque = new ArrayDeque<>();
         boolean[] visited = new boolean[101];
         deque.addLast(node);
-        while(!deque.isEmpty()) {
+        while (!deque.isEmpty()) {
             GraphNode p = deque.removeFirst();
-            if(!visited[p.val]) {
+            if (!visited[p.val]) {
                 visited[p.val] = true;
                 GraphNode newNode = null;
-                if(!map.containsKey(p.val)) {
+                if (!map.containsKey(p.val)) {
                     newNode = new GraphNode(node.val);
                     map.put(newNode.val, newNode);
                 } else {
                     newNode = map.get(p.val);
                 }
 
-                for(GraphNode neighbor : p.neighbors) {
-                    if(map.containsKey(neighbor.val)) {
+                for (GraphNode neighbor : p.neighbors) {
+                    if (map.containsKey(neighbor.val)) {
                         newNode.neighbors.add(map.get(neighbor.val));
                     } else {
                         GraphNode newNeighbor = new GraphNode(neighbor.val);
                         map.put(newNeighbor.val, newNeighbor);
                         newNode.neighbors.add(newNeighbor);
                     }
-                    if(!visited[neighbor.val]) {
+                    if (!visited[neighbor.val]) {
                         deque.addLast(neighbor);
+                    }
+                }
+            }
+        }
+        return map.get(node.val);
+    }
+
+
+    // 三刷
+    public GraphNode cloneGraph_3(GraphNode node) {
+        if (node == null) {
+            return null;
+        }
+        Map<Integer, GraphNode> map = new HashMap<>();
+        Deque<GraphNode> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[101];
+        queue.addLast(node);
+        while (!queue.isEmpty()) {
+            GraphNode cur = queue.removeFirst();
+            if (!visited[cur.val]) {
+                visited[cur.val] = true;
+                GraphNode newNode = null;
+                if (!map.containsKey(cur.val)) {
+                    newNode = new GraphNode(cur.val);
+                    map.put(newNode.val, newNode);
+                } else {
+                    newNode = map.get(cur.val);
+                }
+
+                for (GraphNode neighbour : cur.neighbors) {
+                    if (map.containsKey(neighbour.val)) {
+                        newNode.neighbors.add(map.get(neighbour.val));
+                    } else {
+                        GraphNode newNeighbour = new GraphNode(neighbour.val);
+                        map.put(neighbour.val, newNeighbour);
+                        newNode.neighbors.add(newNeighbour);
+                    }
+                    if (!visited[neighbour.val]) {
+                        queue.add(neighbour);
                     }
                 }
             }
