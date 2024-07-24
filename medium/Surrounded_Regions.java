8262437645a949a3507fd3bb019f7ff67aca84fa
Jingzhe Gao <435390736@qq.com>
2024-07-24 08:06:31 +0800

    130. Surrounded Regions

--- medium/Surrounded_Regions.java
+++ medium/Surrounded_Regions.java
@@ -57,7 +57,7 @@ public class Surrounded_Regions {
         if (j > 0 && board[i][j - 1] == 'O') { // left
             changeNextToBoardO(board, i, j - 1);
         }
-        if (j < board[0].length - 1 && board[i][j + 1] == 'O') { //right
+        if (j < board[0].length - 1 && board[i][j + 1] == 'O') { // right
             changeNextToBoardO(board, i, j + 1);
         }
     }
@@ -116,6 +116,55 @@ public class Surrounded_Regions {
         }
         if (y + 1 < board[0].length && board[x][y + 1] == 'O') {
             convertEdgeOtoZ(board, x, y + 1);
+        }
+    }
+
+
+    // 三刷
+    public void solve_3(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, directions, i, 0, m, n);
+            }
+            if (board[i][n - 1] == 'O') {
+                dfs(board, directions, i, n - 1, m, n);
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') {
+                dfs(board, directions, 0, j, m, n);
+            }
+            if (board[m - 1][j] == 'O') {
+                dfs(board, directions, m - 1, j, m, n);
+            }
+        }
+
+        for (int i = 0; i < m; i++) System.out.println(board[i]);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == 'z') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int[][] directions, int i, int j, int m, int n) {
+        if (i >= 0 && i < m && j >= 0 && j < n && board[i][j] == 'O') {
+            board[i][j] = 'z';
+            for (int idx = 0; idx < 4; idx++) {
+                int x = i + directions[idx][0];
+                int y = j + directions[idx][1];
+                dfs(board, directions, x, y, m, n);
+            }
         }
     }
 }
