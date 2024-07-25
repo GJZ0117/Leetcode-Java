dca50fa7f41e8f7f823c0d415440c81f013c7c95
Jingzhe Gao <435390736@qq.com>
2024-07-25 08:09:27 +0800

    990. Satisfiability of Equality Equations

--- medium/Satisfiability_of_Equality_Equations.java
+++ medium/Satisfiability_of_Equality_Equations.java
@@ -60,4 +60,42 @@ public class Satisfiability_of_Equality_Equations {
             return find(x) == find(y);
         }
     }
+
+
+    // 二刷
+    public boolean equationsPossible_2(String[] equations) {
+        int[] parent = new int[26];
+        for (int i = 0; i < 26; i++) {
+            parent[i] = i;
+        }
+        for (String equation : equations) {
+            if (equation.charAt(1) == '=') {
+                int index1 = equation.charAt(0) - 'a';
+                int index2 = equation.charAt(3) - 'a';
+                union(parent, index1, index2);
+            }
+        }
+        for (String equation : equations) {
+            if (equation.charAt(1) == '!') {
+                int index1 = equation.charAt(0) - 'a';
+                int index2 = equation.charAt(3) - 'a';
+                if (find(parent, index1) == find(parent, index2)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void union(int[] parent, int index1, int index2) {
+        parent[find(parent, index1)] = find(parent, index2);
+    }
+
+    public int find(int[] parent, int index) {
+        while (parent[index] != index) {
+            parent[index] = parent[parent[index]];
+            index = parent[index];
+        }
+        return index;
+    }
 }
