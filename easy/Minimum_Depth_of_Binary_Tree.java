6385014f9276783a9906b87de32d0791ae97e285
Jingzhe Gao <435390736@qq.com>
2024-08-12 07:44:51 +0800

    111. Minimum Depth of Binary Tree

--- easy/Minimum_Depth_of_Binary_Tree.java
+++ easy/Minimum_Depth_of_Binary_Tree.java
@@ -13,12 +13,32 @@ public class Minimum_Depth_of_Binary_Tree {
         }
         int left = Integer.MAX_VALUE;
         int right = Integer.MAX_VALUE;
-        if (root.left!=null){
+        if (root.left != null) {
             left = minDepth(root.left) + 1;
         }
-        if (root.right !=null) {
+        if (root.right != null) {
             right = minDepth(root.right) + 1;
         }
         return Math.min(left, right);
+    }
+
+
+    // 二刷
+    public int minDepth_2(TreeNode root) {
+        preOrder(root, 1);
+        return minDepth == Integer.MAX_VALUE ? 0 : minDepth;
+    }
+
+    int minDepth = Integer.MAX_VALUE;
+
+    public void preOrder(TreeNode node, int curDepth) {
+        if (node == null) {
+            return;
+        }
+        if (node.left == null && node.right == null) {
+            minDepth = Math.min(minDepth, curDepth);
+        }
+        preOrder(node.left, curDepth + 1);
+        preOrder(node.right, curDepth + 1);
     }
 }
