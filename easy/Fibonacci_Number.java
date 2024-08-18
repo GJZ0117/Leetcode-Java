4c57ecdc64231b80ee566a27b9b73cb8685ebd61
Jingzhe Gao <435390736@qq.com>
2024-08-18 09:19:03 +0800

    509. Fibonacci Number

--- easy/Fibonacci_Number.java
+++ easy/Fibonacci_Number.java
@@ -20,4 +20,21 @@ public class Fibonacci_Number {
         }
         return temp;
     }
+
+
+    // 二刷
+    public int fib_2(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int a = 0;
+        int b = 1;
+        int temp = 0;
+        for (int i = 1; i <= n; i++) {
+            temp = a + b;
+            b = a;
+            a = temp;
+        }
+        return temp;
+    }
 }
