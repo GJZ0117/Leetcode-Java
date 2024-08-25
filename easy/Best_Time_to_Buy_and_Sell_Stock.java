7cd61a8e720cb6fa6e599b17945befffcce5f0dd
Jingzhe Gao <435390736@qq.com>
2024-08-26 07:36:36 +0800

    121. Best Time to Buy and Sell Stock

--- easy/Best_Time_to_Buy_and_Sell_Stock.java
+++ easy/Best_Time_to_Buy_and_Sell_Stock.java
@@ -15,15 +15,27 @@ public class Best_Time_to_Buy_and_Sell_Stock {
         int min = prices[0];
         int maxDiff = prices[1] - min;
         int curDiff;
-        for (int i=2; i<prices.length; i++){
-            if (prices[i-1] < min) {
-                min = prices[i-1];
+        for (int i = 2; i < prices.length; i++) {
+            if (prices[i - 1] < min) {
+                min = prices[i - 1];
             }
             curDiff = prices[i] - min;
-            if (curDiff > maxDiff){
+            if (curDiff > maxDiff) {
                 maxDiff = curDiff;
             }
         }
         return Math.max(maxDiff, 0);
+    }
+
+
+    // 二刷
+    public int maxProfit_2(int[] prices) {
+        int cost = Integer.MAX_VALUE;
+        int profit = 0;
+        for (int price : prices) {
+            cost = Math.min(price, cost);
+            profit = Math.max(profit, price - cost);
+        }
+        return profit;
     }
 }
